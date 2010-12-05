8ee33da2411ae03a17f31fd362316914279ce311
nulltoken <emeric.fermas@gmail.com>
2010-12-05 11:07:59 +0100

    Small fix to ObjectType enum in order to avoid too many "magic numbers".

--- libgit2sharp/ObjectType.cs
+++ libgit2sharp/ObjectType.cs
@@ -1,10 +1,12 @@
-﻿namespace libgit2sharp
+﻿using libgit2sharp.Wrapper;
+
+namespace libgit2sharp
 {
     public enum ObjectType
     {
-        Commit = 1,     /**< A commit object. */
-        Tree = 2,       /**< A tree (directory listing) object. */
-        Blob = 3,       /**< A file revision object. */
-        Tag = 4,        /**< An annotated tag object. */
+        Commit = git_otype.GIT_OBJ_COMMIT,     /**< A commit object. */
+        Tree = git_otype.GIT_OBJ_TREE,       /**< A tree (directory listing) object. */
+        Blob = git_otype.GIT_OBJ_BLOB,       /**< A file revision object. */
+        Tag = git_otype.GIT_OBJ_TAG,        /**< An annotated tag object. */
     }
 }
