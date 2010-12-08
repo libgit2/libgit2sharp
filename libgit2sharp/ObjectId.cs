f3c93ee9e247b70e2454919774fec3f6a2d59733
nulltoken <emeric.fermas@gmail.com>
2010-12-08 06:29:40 +0100

    Added ObjectId byte[] to string conversion utility.

---
+++ libgit2sharp/ObjectId.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using libgit2sharp.Wrapper;
+
+namespace libgit2sharp
+{
+    public static class ObjectId
+    {
+        private static readonly char[] hexDigits = new []
+                                                       {
+                                                           '0', '1', '2', '3', '4', '5', '6', '7', '8', 
+                                                           '9', 'a', 'b', 'c', 'd', 'e', 'f'
+                                                       };
+
+        public static string ToString(byte[] id)
+        {
+            Debug.Assert(id != null && id.Length == Constants.GIT_OID_RAWSZ);
+
+            // Inspired from http://stackoverflow.com/questions/623104/c-byte-to-hex-string/3974535#3974535
+
+            var c = new char[Constants.GIT_OID_RAWSZ * 2];
+
+            for (int i = 0; i < Constants.GIT_OID_RAWSZ * 2; i++)
+            {
+                int index0 = i >> 1;
+                var b = ((byte)(id[index0] >> 4));
+                c[i++] = hexDigits[b];
+
+                b = ((byte)(id[index0] & 0x0F));
+                c[i] = hexDigits[b];
+            }
+
+            return new string(c);
+        }
+    }
+}
--- libgit2sharp/Wrapper/Constants.cs
+++ libgit2sharp/Wrapper/Constants.cs
@@ -4,6 +4,6 @@
     {
         //public const int GIT_PATH_MAX = 4096;
         public const int GIT_ERROR = -1;
-
+        public const int GIT_OID_RAWSZ = 20;
     }
 }
