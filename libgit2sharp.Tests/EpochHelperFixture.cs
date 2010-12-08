1267c6414fd8cf7f03713556fc7982c8a34d8c69
nulltoken <emeric.fermas@gmail.com>
2010-12-08 11:34:32 +0100

    Added EpochHelper class to ease unix time/DateTimeOffset conversion.

---
+++ libgit2sharp.Tests/EpochHelperFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace libgit2sharp.Tests
+{
+    [TestFixture]
+    public class EpochHelperFixture
+    {
+        [TestCase(0)]
+        [TestCase(17)]
+        public void ToDateDateTimeOffset_ShouldReturnAUtcBasedDateTimeOffset(Int32 secondsSinceEpoch)
+        {
+            DateTimeOffset when = EpochHelper.ToDateTimeOffset(secondsSinceEpoch);
+            Assert.AreEqual(TimeSpan.Zero, when.Offset);
+            Assert.AreEqual(DateTimeKind.Utc, when.UtcDateTime.Kind);
+        }
+
+        [TestCase(1291801952, "Wed, 08 Dec 2010 09:52:32 GMT")]
+        [TestCase(1234567890, "Fri, 13 Feb 2009 23:31:30 GMT")]
+        public void ToDateDateTimeOffset_ShouldCorrectlyConvert(Int32 secondsSinceEpoch, string expected)
+        {
+            DateTimeOffset when = EpochHelper.ToDateTimeOffset(secondsSinceEpoch);
+            var expectedDate = DateTimeOffset.Parse(expected);
+            Assert.AreEqual(expectedDate, when);
+        }
+    }
+}
---
+++ libgit2sharp/EpochHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace libgit2sharp
+{
+    public static class EpochHelper
+    {
+        public static readonly long EpochTicks = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero).Ticks;
+
+        public static DateTimeOffset ToDateTimeOffset(Int32 secondsSinceEpoch)
+        {
+            long ticks = secondsSinceEpoch * TimeSpan.TicksPerSecond + EpochTicks;
+            return new DateTimeOffset(ticks, TimeSpan.Zero);
+        }
+    }
+}
