754b296db96a1fe1e22b091b7b4401c60f3d2c48
nulltoken <emeric.fermas@gmail.com>
2010-12-10 17:56:54 +0100

    Added GitDate type which represents a Git internal date (unix timestamp and timezone offset).

---
+++ libgit2sharp/GitDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace libgit2sharp
+{
+    public class GitDate
+    {
+        public GitDate(int secondsSinceEpoch, int timeZoneOffsetInMinutes)
+        {
+            UnixTimeStamp = secondsSinceEpoch;
+            TimeZoneOffset = timeZoneOffsetInMinutes;
+        }
+
+        public Int32 UnixTimeStamp { get; private set; }
+        public Int32 TimeZoneOffset { get; private set; }
+    }
+}
