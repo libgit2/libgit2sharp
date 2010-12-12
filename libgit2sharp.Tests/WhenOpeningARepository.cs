2c38638d4b1de699f895ae60155f41bc92cc34c7
nulltoken <emeric.fermas@gmail.com>
2010-12-12 20:56:42 +0100

    Added some tests covering the instantiation of a repository and the parsing of provided paths.

---
+++ libgit2sharp.Tests/WhenOpeningARepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace libgit2sharp.Tests
+{
+    [TestFixture]
+    public class InstanciatingARepository
+    {
+        const string PathToRepository = "../../Resources/testrepo.git";
+
+        [Test]
+        [Ignore("https://github.com/libgit2/libgit2/issues/issue/28")]
+        public void ShouldThrowIfPassedANonValidGitDirectory()
+        {
+            var notAValidRepo = Path.GetTempPath();
+            var exception = Assert.Throws<Exception>(() => new Repository(notAValidRepo));
+            Assert.Fail("To be finalized.");
+        }
+
+        [Test]
+        [Ignore("https://github.com/libgit2/libgit2/issues/issue/28")]
+        public void ShouldThrowIfPassedANonExistingFolder()
+        {
+            var notAValidRepo = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Guid.NewGuid().ToString());
+            var exception = Assert.Throws<Exception>(() => new Repository(notAValidRepo));
+            Assert.Fail("To be finalized.");
+        }
+
+        [Test]
+        [Ignore]
+        public void ShouldAllowPassedRelativeBackslashedPath()
+        {
+            Assert.Fail("To be finalized.");
+        }
+
+        [Test]
+        [Ignore]
+        public void ShouldAllowPassedRelativeBackslahedPathWithaTrailingBackslash()
+        {
+            Assert.Fail("To be finalized.");
+        }
+
+    
+    }
+}
