99acd3c216fed578c9551166d852b2705cfca095
Tim Clem <timothy.clem@gmail.com>
2011-03-30 11:24:08 -0700

    add docs to signature

--- LibGit2Sharp/Signature.cs
+++ LibGit2Sharp/Signature.cs
@@ -3,6 +3,9 @@ using System.Runtime.InteropServices;
 
 namespace LibGit2Sharp
 {
+    /// <summary>
+    ///   A signature
+    /// </summary>
     public class Signature
     {
         private readonly GitSignature sig = new GitSignature();
@@ -17,21 +20,36 @@ namespace LibGit2Sharp
             }
         }
 
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "Signature" /> class.
+        /// </summary>
+        /// <param name = "name">The name.</param>
+        /// <param name = "email">The email.</param>
+        /// <param name = "when">The when.</param>
         public Signature(string name, string email, DateTimeOffset when)
             : this(NativeMethods.git_signature_new(name, email, when.ToSecondsSinceEpoch(), (int) when.Offset.TotalMinutes), false)
         {
         }
 
+        /// <summary>
+        ///   Gets the name.
+        /// </summary>
         public string Name
         {
             get { return sig.Name; }
         }
 
+        /// <summary>
+        ///   Gets the email.
+        /// </summary>
         public string Email
         {
             get { return sig.Email; }
         }
 
+        /// <summary>
+        ///   Gets the date when this signature happened.
+        /// </summary>
         public DateTimeOffset When
         {
             get
