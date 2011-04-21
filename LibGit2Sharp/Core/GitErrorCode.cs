39375d00d6c09a08f3669a7bb1b3f6eb20e4796b
nulltoken <emeric.fermas@gmail.com>
2011-04-21 22:00:44 +0200

    Fix malformed Xml documentation

--- LibGit2Sharp/Core/GitErrorCode.cs
+++ LibGit2Sharp/Core/GitErrorCode.cs
@@ -2,84 +2,132 @@
 {
     internal enum GitErrorCode
     {
-        /** Operation completed successfully. */
+        /// <summary>
+        /// Operation completed successfully.
+        /// </summary>
         GIT_SUCCESS = 0,
 
-        /**
-         * Operation failed, with unspecified reason.
-         * This value also serves as the base error code; all other
-         * error codes are subtracted from it such that all errors
-         * are < 0, in typical POSIX C tradition.
-         */
+        /// <summary>
+        /// Operation failed, with unspecified reason.
+        /// This value also serves as the base error code; all other
+        /// error codes are subtracted from it such that all errors
+        /// are &lt; 0, in typical POSIX C tradition.
+        /// </summary>
         GIT_ERROR = -1,
 
-        /** Input was not a properly formatted Git object id. */
+        /// <summary>
+        /// Input was not a properly formatted Git object id.
+        /// </summary>
         GIT_ENOTOID = (GIT_ERROR - 1),
 
-        /** Input does not exist in the scope searched. */
+        /// <summary>
+        /// Input does not exist in the scope searched.
+        /// </summary>
         GIT_ENOTFOUND = (GIT_ERROR - 2),
 
-        /** Not enough space available. */
+        /// <summary>
+        /// Not enough space available.
+        /// </summary>
         GIT_ENOMEM = (GIT_ERROR - 3),
 
-        /** Consult the OS error information. */
+        /// <summary>
+        /// Consult the OS error information.
+        /// </summary>
         GIT_EOSERR = (GIT_ERROR - 4),
 
-        /** The specified object is of invalid type */
+        /// <summary>
+        /// The specified object is of invalid type
+        /// </summary>
         GIT_EOBJTYPE = (GIT_ERROR - 5),
 
-        /** The specified object has its data corrupted */
+        /// <summary>
+        /// The specified object has its data corrupted
+        /// </summary>
         GIT_EOBJCORRUPTED = (GIT_ERROR - 6),
 
-        /** The specified repository is invalid */
+        /// <summary>
+        /// The specified repository is invalid
+        /// </summary>
         GIT_ENOTAREPO = (GIT_ERROR - 7),
 
-        /** The object type is invalid or doesn't match */
+        /// <summary>
+        /// The object type is invalid or doesn't match
+        /// </summary>
         GIT_EINVALIDTYPE = (GIT_ERROR - 8),
 
-        /** The object cannot be written that because it's missing internal data */
+        /// <summary>
+        /// The object cannot be written that because it's missing internal data
+        /// </summary>
         GIT_EMISSINGOBJDATA = (GIT_ERROR - 9),
 
-        /** The packfile for the ODB is corrupted */
+        /// <summary>
+        /// The packfile for the ODB is corrupted
+        /// </summary>
         GIT_EPACKCORRUPTED = (GIT_ERROR - 10),
 
-        /** Failed to adquire or release a file lock */
+        /// <summary>
+        /// Failed to adquire or release a file lock
+        /// </summary>
         GIT_EFLOCKFAIL = (GIT_ERROR - 11),
 
-        /** The Z library failed to inflate/deflate an object's data */
+        /// <summary>
+        /// The Z library failed to inflate/deflate an object's data
+        /// </summary>
         GIT_EZLIB = (GIT_ERROR - 12),
 
-        /** The queried object is currently busy */
+        /// <summary>
+        /// The queried object is currently busy
+        /// </summary>
         GIT_EBUSY = (GIT_ERROR - 13),
 
-        /** The index file is not backed up by an existing repository */
+        /// <summary>
+        /// The index file is not backed up by an existing repository
+        /// </summary>
         GIT_EBAREINDEX = (GIT_ERROR - 14),
 
-        /** The name of the reference is not valid */
+        /// <summary>
+        /// The name of the reference is not valid
+        /// </summary>
         GIT_EINVALIDREFNAME = (GIT_ERROR - 15),
 
-        /** The specified reference has its data corrupted */
+        /// <summary>
+        /// The specified reference has its data corrupted
+        /// </summary>
         GIT_EREFCORRUPTED = (GIT_ERROR - 16),
 
-        /** The specified symbolic reference is too deeply nested */
+        /// <summary>
+        /// The specified symbolic reference is too deeply nested
+        /// </summary>
         GIT_ETOONESTEDSYMREF = (GIT_ERROR - 17),
 
-        /** The pack-refs file is either corrupted of its format is not currently supported */
+        /// <summary>
+        /// The pack-refs file is either corrupted of its format is not currently supported
+        /// </summary>
         GIT_EPACKEDREFSCORRUPTED = (GIT_ERROR - 18),
 
-        /** The path is invalid */
+        /// <summary>
+        /// The path is invalid
+        /// </summary>
         GIT_EINVALIDPATH = (GIT_ERROR - 19),
 
-        /** The revision walker is empty; there are no more commits left to iterate */
+        /// <summary>
+        /// The revision walker is empty; there are no more commits left to iterate
+        /// </summary>
         GIT_EREVWALKOVER = (GIT_ERROR - 20),
 
-        /** The state of the reference is not valid */
+        /// <summary>
+        /// The state of the reference is not valid
+        /// </summary>
         GIT_EINVALIDREFSTATE = (GIT_ERROR - 21),
 
-        /** This feature has not been implemented yet */
+        /// <summary>
+        /// This feature has not been implemented yet
+        /// </summary>
         GIT_ENOTIMPLEMENTED = (GIT_ERROR - 22),
 
-        /** A reference with this name already exists */
+        /// <summary>
+        /// A reference with this name already exists
+        /// </summary>
         GIT_EEXISTS = (GIT_ERROR - 23),
     }
 }
