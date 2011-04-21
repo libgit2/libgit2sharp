6063d86a9265ca0c04630ea7de2693477b1fc60a
nulltoken <emeric.fermas@gmail.com>
2011-04-21 21:26:25 +0200

    Add TreeEntry equality members

--- LibGit2Sharp/TreeEntry.cs
+++ LibGit2Sharp/TreeEntry.cs
@@ -4,11 +4,14 @@ using LibGit2Sharp.Core;
 
 namespace LibGit2Sharp
 {
-    public class TreeEntry
+    public class TreeEntry : IEquatable<TreeEntry>
     {
         private readonly Repository repo;
         private GitObject target;
         private readonly ObjectId targetOid;
+
+        private static readonly LambdaEqualityHelper<TreeEntry> equalityHelper =
+            new LambdaEqualityHelper<TreeEntry>(new Func<TreeEntry, object>[] { x => x.Name, x => x.targetOid }); //TODO: Replace the target oid with the parent Tree oid
 
         public TreeEntry(IntPtr obj, Repository repo)
         {
@@ -29,8 +32,61 @@ namespace LibGit2Sharp
         private GitObject RetreiveTreeEntryTarget()
         {
             GitObject treeEntryTarget = repo.Lookup(targetOid);
+
             Ensure.ArgumentConformsTo(treeEntryTarget.GetType(), t => typeof(Blob).IsAssignableFrom(t) || typeof(Tree).IsAssignableFrom(t), "treeEntryTarget");
+            
             return treeEntryTarget;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Object"/> is equal to the current <see cref="TreeEntry"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="Object"/> to compare with the current <see cref="TreeEntry"/>.</param>
+        /// <returns>True if the specified <see cref="Object"/> is equal to the current <see cref="TreeEntry"/>; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TreeEntry);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="TreeEntry"/> is equal to the current <see cref="TreeEntry"/>.
+        /// </summary>
+        /// <param name="other">The <see cref="TreeEntry"/> to compare with the current <see cref="TreeEntry"/>.</param>
+        /// <returns>True if the specified <see cref="TreeEntry"/> is equal to the current <see cref="TreeEntry"/>; otherwise, false.</returns>
+        public bool Equals(TreeEntry other)
+        {
+            return equalityHelper.Equals(this, other);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return equalityHelper.GetHashCode(this);
+        }
+
+        /// <summary>
+        /// Tests if two <see cref="TreeEntry"/> are equal.
+        /// </summary>
+        /// <param name="left">First <see cref="TreeEntry"/> to compare.</param>
+        /// <param name="right">Second <see cref="TreeEntry"/> to compare.</param>
+        /// <returns>True if the two objects are equal; false otherwise.</returns>
+        public static bool operator ==(TreeEntry left, TreeEntry right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        /// Tests if two <see cref="TreeEntry"/> are different.
+        /// </summary>
+        /// <param name="left">First <see cref="TreeEntry"/> to compare.</param>
+        /// <param name="right">Second <see cref="TreeEntry"/> to compare.</param>
+        /// <returns>True if the two objects are different; false otherwise.</returns>
+        public static bool operator !=(TreeEntry left, TreeEntry right)
+        {
+            return !Equals(left, right);
         }
     }
 }
