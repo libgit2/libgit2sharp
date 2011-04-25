c78487d2068cfa15c09ab54057ca1e6f449adb06
Tim Clem <timothy.clem@gmail.com>
2011-04-25 15:10:49 -0700

    move RepositorySafeHandle into core and reformat

---
+++ LibGit2Sharp/Core/RepositorySafeHandle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibGit2Sharp.Core
+{
+    public class RepositorySafeHandle : SafeHandle
+    {
+        public RepositorySafeHandle() : base(IntPtr.Zero, true)
+        {
+        }
+
+        public override bool IsInvalid
+        {
+            get { return (handle == IntPtr.Zero); }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            NativeMethods.git_repository_free(handle);
+            return true;
+        }
+    }
+}
