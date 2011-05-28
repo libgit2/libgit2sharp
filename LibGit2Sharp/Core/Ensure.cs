bcb1feb530dd8c6958445841395d0cdb4f38adbd
nulltoken <emeric.fermas@gmail.com>
2011-05-28 07:11:34 +0200

    Downgrade libgit2 binaries to cd2cc2d
    This should solve (not explain) an issue with the CI server complaining about AccessViolationException.
    The problem can be reproduced with libgit2 binaries 5711ca9. The root cause has yet to be analyzed.

--- LibGit2Sharp/Core/Ensure.cs
+++ LibGit2Sharp/Core/Ensure.cs
@@ -46,8 +46,8 @@ namespace LibGit2Sharp.Core
             {
                 return;
             }
-            
-            string errorMessage = NativeMethods.git_lasterror().MarshallAsString();
+
+            string errorMessage = "An error occured"; // NativeMethods.git_lasterror().MarshallAsString();
 
             throw new ApplicationException(
                 String.Format(CultureInfo.InvariantCulture, "An error was raised by libgit2. Error code = {0} ({1}).{2}{3}", Enum.GetName(typeof(GitErrorCode), result), result, Environment.NewLine, errorMessage));
--- LibGit2Sharp/Core/GitErrorCode.cs
+++ LibGit2Sharp/Core/GitErrorCode.cs
@@ -18,136 +18,136 @@
         /// <summary>
         /// Input was not a properly formatted Git object id.
         /// </summary>
-        GIT_ENOTOID = -2,
+        GIT_ENOTOID = (GIT_ERROR - 1),
 
         /// <summary>
         /// Input does not exist in the scope searched.
         /// </summary>
-        GIT_ENOTFOUND = -3,
+        GIT_ENOTFOUND = (GIT_ERROR - 2),
 
         /// <summary>
         /// Not enough space available.
         /// </summary>
-        GIT_ENOMEM = -4,
+        GIT_ENOMEM = (GIT_ERROR - 3),
 
         /// <summary>
         /// Consult the OS error information.
         /// </summary>
-        GIT_EOSERR = -5,
+        GIT_EOSERR = (GIT_ERROR - 4),
 
         /// <summary>
         /// The specified object is of invalid type
         /// </summary>
-        GIT_EOBJTYPE = -6,
-
-        /// <summary>
-        /// The specified repository is invalid
-        /// </summary>
-        GIT_ENOTAREPO = -7,
-
-        /// <summary>
-        /// The object type is invalid or doesn't match
-        /// </summary>
-        GIT_EINVALIDTYPE = -8,
-
-        /// <summary>
-        /// The object cannot be written that because it's missing internal data
-        /// </summary>
-        GIT_EMISSINGOBJDATA = -9,
-
-        /// <summary>
-        /// The packfile for the ODB is corrupted
-        /// </summary>
-        GIT_EPACKCORRUPTED = -10,
-
-        /// <summary>
-        /// Failed to adquire or release a file lock
-        /// </summary>
-        GIT_EFLOCKFAIL = -11,
-
-        /// <summary>
-        /// The Z library failed to inflate/deflate an object's data
-        /// </summary>
-        GIT_EZLIB = -12,
-
-        /// <summary>
-        /// The queried object is currently busy
-        /// </summary>
-        GIT_EBUSY = -13,
-
-        /// <summary>
-        /// The index file is not backed up by an existing repository
-        /// </summary>
-        GIT_EBAREINDEX = -14,
-
-        /// <summary>
-        /// The name of the reference is not valid
-        /// </summary>
-        GIT_EINVALIDREFNAME = -15,
-
-        /// <summary>
-        /// The specified reference has its data corrupted
-        /// </summary>
-        GIT_EREFCORRUPTED = -16,
-
-        /// <summary>
-        /// The specified symbolic reference is too deeply nested
-        /// </summary>
-        GIT_ETOONESTEDSYMREF = -17,
-
-        /// <summary>
-        /// The pack-refs file is either corrupted of its format is not currently supported
-        /// </summary>
-        GIT_EPACKEDREFSCORRUPTED = -18,
-
-        /// <summary>
-        /// The path is invalid
-        /// </summary>
-        GIT_EINVALIDPATH = -19,
-
-        /// <summary>
-        /// The revision walker is empty; there are no more commits left to iterate
-        /// </summary>
-        GIT_EREVWALKOVER = -20,
-
-        /// <summary>
-        /// The state of the reference is not valid
-        /// </summary>
-        GIT_EINVALIDREFSTATE = -21,
-
-        /// <summary>
-        /// This feature has not been implemented yet
-        /// </summary>
-        GIT_ENOTIMPLEMENTED = -22,
-
-        /// <summary>
-        /// A reference with this name already exists
-        /// </summary>
-        GIT_EEXISTS = -23,
-
-        /// <summary>
-        /// The given integer literal is too large to be parsed
-        /// </summary>
-        GIT_EOVERFLOW = -24,
-
-        /// <summary>
-        /// The given literal is not a valid number
-        /// </summary>
-        GIT_ENOTNUM = -25,
-
-        /// <summary>
-        /// Streaming error
-        /// </summary>
-        GIT_ESTREAM = -26,
-
-        /// <summary>
-        /// invalid arguments to function
-        /// </summary>
-        GIT_EINVALIDARGS = -27,
+        GIT_EOBJTYPE = (GIT_ERROR - 5),
 
         /// <summary>
         /// The specified object has its data corrupted
         /// </summary>
-        GIT_EOBJCORRUPTED = -28,
+        GIT_EOBJCORRUPTED = (GIT_ERROR - 6),
+
+        /// <summary>
+        /// The specified repository is invalid
+        /// </summary>
+        GIT_ENOTAREPO = (GIT_ERROR - 7),
+
+        /// <summary>
+        /// The object type is invalid or doesn't match
+        /// </summary>
+        GIT_EINVALIDTYPE = (GIT_ERROR - 8),
+
+        /// <summary>
+        /// The object cannot be written that because it's missing internal data
+        /// </summary>
+        GIT_EMISSINGOBJDATA = (GIT_ERROR - 9),
+
+        /// <summary>
+        /// The packfile for the ODB is corrupted
+        /// </summary>
+        GIT_EPACKCORRUPTED = (GIT_ERROR - 10),
+
+        /// <summary>
+        /// Failed to adquire or release a file lock
+        /// </summary>
+        GIT_EFLOCKFAIL = (GIT_ERROR - 11),
+
+        /// <summary>
+        /// The Z library failed to inflate/deflate an object's data
+        /// </summary>
+        GIT_EZLIB = (GIT_ERROR - 12),
+
+        /// <summary>
+        /// The queried object is currently busy
+        /// </summary>
+        GIT_EBUSY = (GIT_ERROR - 13),
+
+        /// <summary>
+        /// The index file is not backed up by an existing repository
+        /// </summary>
+        GIT_EBAREINDEX = (GIT_ERROR - 14),
+
+        /// <summary>
+        /// The name of the reference is not valid
+        /// </summary>
+        GIT_EINVALIDREFNAME = (GIT_ERROR - 15),
+
+        /// <summary>
+        /// The specified reference has its data corrupted
+        /// </summary>
+        GIT_EREFCORRUPTED = (GIT_ERROR - 16),
+
+        /// <summary>
+        /// The specified symbolic reference is too deeply nested
+        /// </summary>
+        GIT_ETOONESTEDSYMREF = (GIT_ERROR - 17),
+
+        /// <summary>
+        /// The pack-refs file is either corrupted of its format is not currently supported
+        /// </summary>
+        GIT_EPACKEDREFSCORRUPTED = (GIT_ERROR - 18),
+
+        /// <summary>
+        /// The path is invalid
+        /// </summary>
+        GIT_EINVALIDPATH = (GIT_ERROR - 19),
+
+        /// <summary>
+        /// The revision walker is empty; there are no more commits left to iterate
+        /// </summary>
+        GIT_EREVWALKOVER = (GIT_ERROR - 20),
+
+        /// <summary>
+        /// The state of the reference is not valid
+        /// </summary>
+        GIT_EINVALIDREFSTATE = (GIT_ERROR - 21),
+
+        /// <summary>
+        /// This feature has not been implemented yet
+        /// </summary>
+        GIT_ENOTIMPLEMENTED = (GIT_ERROR - 22),
+
+        /// <summary>
+        /// A reference with this name already exists
+        /// </summary>
+        GIT_EEXISTS = (GIT_ERROR - 23),
+
+        /// <summary>
+        /// The given integer literal is too large to be parsed
+        /// </summary>
+        GIT_EOVERFLOW = (GIT_ERROR - 24),
+
+        /// <summary>
+        /// The given literal is not a valid number
+        /// </summary>
+        GIT_ENOTNUM = (GIT_ERROR - 25),
+
+        /// <summary>
+        /// Streaming error
+        /// </summary>
+        GIT_ESTREAM = (GIT_ERROR - 26),
+
+        /// <summary>
+        /// invalid arguments to function
+        /// </summary>
+        GIT_EINVALIDARGS = (GIT_ERROR - 27),
     }
 }
