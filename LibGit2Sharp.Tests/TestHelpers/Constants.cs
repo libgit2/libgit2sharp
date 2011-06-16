649586d0a2c9e17c30f3a92dc66679dca89d1aee
nulltoken <emeric.fermas@gmail.com>
2011-06-16 11:25:43 +0200

    Add Signature related test helpers

--- LibGit2Sharp.Tests/TestHelpers/Constants.cs
+++ LibGit2Sharp.Tests/TestHelpers/Constants.cs
@@ -1,4 +1,6 @@
-﻿namespace LibGit2Sharp.Tests.TestHelpers
+﻿using System;
+
+namespace LibGit2Sharp.Tests.TestHelpers
 {
     public static class Constants
     {
@@ -7,5 +9,6 @@
         public const string TestRepoWithWorkingDirPath = TestRepoWithWorkingDirRootPath + "/.git";
         public const string TemporaryReposPath = "TestRepos";
         public const string UnknownSha = "deadbeefdeadbeefdeadbeefdeadbeefdeadbeef";
+        public static readonly Signature Signature = new Signature("A. U. Thor", "thor@valhalla.asgard.com", new DateTimeOffset(2011, 06, 16, 10, 58, 27, TimeSpan.FromHours(2)));
     }
 }
---
+++ LibGit2Sharp.Tests/TestHelpers/SignatureExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibGit2Sharp.Tests.TestHelpers
+{
+    public static class SignatureExtensions
+    {
+        public static Signature TimeShift(this Signature signature, TimeSpan shift)
+        {
+            return new Signature(signature.Name, signature.Email, signature.When.Add(shift));
+        }
+    }
+}
