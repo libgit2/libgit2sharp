50fc16105793cfce15ee8eb0f9c0de70646cfc75
nulltoken <emeric.fermas@gmail.com>
2011-07-15 19:26:16 +0200

    Release LibGit2Sharp v0.4.0

--- LibGit2Sharp/Properties/AssemblyInfo.cs
+++ LibGit2Sharp/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
