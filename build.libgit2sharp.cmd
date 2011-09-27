ed9e216036529772f3d7e976448c8843145ce811
nulltoken <emeric.fermas@gmail.com>
2011-09-27 11:47:39 +0200

    Upgrade NuGet to v1.5
     - Make the packaging depend on the build success
     - Add the libgit2 .pdb file to the package
    
    Should fix issue #63.

--- build.libgit2sharp.cmd
+++ build.libgit2sharp.cmd
@@ -1,7 +1,11 @@
+SETLOCAL
+
 SET BASEDIR=%~dp0
-set FrameworkVersion=v4.0.30319
-set FrameworkDir=%SystemRoot%\Microsoft.NET\Framework
+SET FrameworkVersion=v4.0.30319
+SET FrameworkDir=%SystemRoot%\Microsoft.NET\Framework
 
 "%FrameworkDir%\%FrameworkVersion%\msbuild.exe" "%BASEDIR%CI-build.msbuild"
 
-exit /B %ERRORLEVEL%
+ENDLOCAL
+
+EXIT /B %ERRORLEVEL%
