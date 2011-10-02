c1a8e5f423783e866a01ef1facaa32da70daec1b
Keith Dahlby <dahlbyk@gmail.com>
2011-10-02 18:50:24 -0500

    Add FilePath and FilePathMarshaler

---
+++ LibGit2Sharp/Core/FilePath.cs
@@ -0,0 +1,34 @@
+﻿namespace LibGit2Sharp.Core
+{
+    internal class FilePath
+    {
+        private readonly string native;
+        private readonly string posix;
+
+        private FilePath(string path)
+        {
+            native = PosixPathHelper.ToNative(path);
+            posix = PosixPathHelper.ToPosix(path);
+        }
+
+        public string Native
+        {
+            get { return native; }
+        }
+
+        public string Posix
+        {
+            get { return posix; }
+        }
+
+        public override string ToString()
+        {
+            return Native;
+        }
+
+        public static implicit operator FilePath(string path)
+        {
+            return path == null ? null : new FilePath(path);
+        }
+    }
+}
