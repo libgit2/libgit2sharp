69500d71ff3789d77eecaaafe884554ffe9328c2
Artem Smirnov <uluhonolulu@gmail.com>
2012-01-22 17:58:41 +0400

    Created IndexerSafeHandle class

---
+++ LibGit2Sharp/Core/IndexerSafeHandle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibGit2Sharp.Core {
+	internal class IndexerSafeHandle : SafeHandleBase {
+		protected override bool ReleaseHandle() {
+			NativeMethods.git_indexer_free(handle);
+			return true;
+		}
+	}
+}
