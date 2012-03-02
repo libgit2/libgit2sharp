235ccd21cf3641fc540237c1e063c796ebee4cbc
nulltoken <emeric.fermas@gmail.com>
2012-03-02 16:15:25 +0100

    Remove useless namespace imports

--- LibGit2Sharp.Tests/TestHelpers/BaseFixture.cs
+++ LibGit2Sharp.Tests/TestHelpers/BaseFixture.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Xunit;
 
 namespace LibGit2Sharp.Tests.TestHelpers
 {
--- LibGit2Sharp/TagAnnotation.cs
+++ LibGit2Sharp/TagAnnotation.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using LibGit2Sharp.Core;
 using LibGit2Sharp.Core.Compat;
 
