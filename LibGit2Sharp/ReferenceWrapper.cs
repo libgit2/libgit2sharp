e6ba4c9d438aea285fff08d30f6f2cf5925348e2
nulltoken <emeric.fermas@gmail.com>
2012-03-02 16:41:53 +0100

    Refactor implementation of equality members of Branch and Tag types

--- LibGit2Sharp/ReferenceWrapper.cs
+++ LibGit2Sharp/ReferenceWrapper.cs
@@ -8,18 +8,18 @@ namespace LibGit2Sharp
     ///   A base class for things that wrap a <see cref = "Reference" /> (branch, tag, etc).
     /// </summary>
     /// <typeparam name="TObject">The type of the referenced Git object.</typeparam>
-    public abstract class ReferenceWrapper<TObject> where TObject : GitObject
+    public abstract class ReferenceWrapper<TObject> : IEquatable<ReferenceWrapper<TObject>> where TObject : GitObject
     {
         /// <summary>
-        ///   The repo.
+        ///   The repository.
         /// </summary>
         protected readonly Repository repo;
         private readonly Lazy<TObject> objectBuilder;
 
-        /// <summary>
-        ///   Initializes a new instance of the <see cref = "ReferenceWrapper{TObject}" /> class.
-        /// </summary>
-        /// <param name="repo">The repo.</param>
+        private static readonly LambdaEqualityHelper<ReferenceWrapper<TObject>> equalityHelper =
+            new LambdaEqualityHelper<ReferenceWrapper<TObject>>(new Func<ReferenceWrapper<TObject>, object>[] { x => x.CanonicalName, x => x.TargetObject });
+
+        /// <param name="repo">The repository.</param>
         /// <param name="reference">The reference.</param>
         /// <param name="canonicalNameSelector">A function to construct the reference's canonical name.</param>
         protected internal ReferenceWrapper(Repository repo, Reference reference, Func<Reference, string> canonicalNameSelector)
@@ -86,6 +86,57 @@ namespace LibGit2Sharp
             }
 
             return repo.Lookup<TObject>(target.Id);
+        }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref = "ReferenceWrapper{TObject}" /> is equal to the current <see cref = "ReferenceWrapper{TObject}" />.
+        /// </summary>
+        /// <param name = "other">The <see cref = "ReferenceWrapper{TObject}" /> to compare with the current <see cref = "ReferenceWrapper{TObject}" />.</param>
+        /// <returns>True if the specified <see cref = "ReferenceWrapper{TObject}" /> is equal to the current <see cref = "ReferenceWrapper{TObject}" />; otherwise, false.</returns>
+        public bool Equals(ReferenceWrapper<TObject> other)
+        {
+            return equalityHelper.Equals(this, other);
+        }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref = "Object" /> is equal to the current <see cref = "ReferenceWrapper{TObject}" />.
+        /// </summary>
+        /// <param name = "obj">The <see cref = "Object" /> to compare with the current <see cref = "ReferenceWrapper{TObject}" />.</param>
+        /// <returns>True if the specified <see cref = "Object" /> is equal to the current <see cref = "ReferenceWrapper{TObject}" />; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ReferenceWrapper<TObject>);
+        }
+
+        /// <summary>
+        ///   Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return equalityHelper.GetHashCode(this);
+        }
+
+        /// <summary>
+        ///   Tests if two <see cref = "ReferenceWrapper{TObject}" /> are equal.
+        /// </summary>
+        /// <param name = "left">First <see cref = "ReferenceWrapper{TObject}" /> to compare.</param>
+        /// <param name = "right">Second <see cref = "ReferenceWrapper{TObject}" /> to compare.</param>
+        /// <returns>True if the two objects are equal; false otherwise.</returns>
+        public static bool operator ==(ReferenceWrapper<TObject> left, ReferenceWrapper<TObject> right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        ///   Tests if two <see cref = "ReferenceWrapper{TObject}" /> are different.
+        /// </summary>
+        /// <param name = "left">First <see cref = "ReferenceWrapper{TObject}" /> to compare.</param>
+        /// <param name = "right">Second <see cref = "ReferenceWrapper{TObject}" /> to compare.</param>
+        /// <returns>True if the two objects are different; false otherwise.</returns>
+        public static bool operator !=(ReferenceWrapper<TObject> left, ReferenceWrapper<TObject> right)
+        {
+            return !Equals(left, right);
         }
     }
 }
