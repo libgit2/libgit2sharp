8c8e187046db12054f826564274bc86af8c304e6
Haacked <haacked@gmail.com>
2012-03-09 15:13:18 -0800

    Changing DetachedHead to public

--- LibGit2Sharp/DetachedHead.cs
+++ LibGit2Sharp/DetachedHead.cs
@@ -1,8 +1,8 @@
 ﻿namespace LibGit2Sharp
 {
-    internal class DetachedHead : Branch
+    public class DetachedHead : Branch
     {
-        internal DetachedHead(Repository repo, Reference reference)
+        public DetachedHead(Repository repo, Reference reference)
             : base(repo, reference, "(no branch)")
         {
         }
