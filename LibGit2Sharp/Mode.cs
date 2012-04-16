0787252beb87eb9e7a707e6c414c533b9f8bc38f
nulltoken <emeric.fermas@gmail.com>
2012-04-16 21:45:27 +0200

    Add new Mode enum

---
+++ LibGit2Sharp/Mode.cs
@@ -0,0 +1,40 @@
+﻿namespace LibGit2Sharp
+{
+    /// <summary>
+    ///   Git specific modes for entries.
+    /// </summary>
+    public enum Mode
+    {
+        // Inspired from http://stackoverflow.com/a/8347325/335418
+
+        /// <summary>
+        ///   040000 file mode
+        /// </summary>
+        Directory = 0x4000,
+
+        /// <summary>
+        ///   100644 file mode
+        /// </summary>
+        NonExecutableFile = 0x81A4,
+
+        /// <summary>
+        ///   100664 file mode
+        /// </summary>
+        NonExecutableGroupWriteableFile = 0x81B4,
+
+        /// <summary>
+        ///   100755 file mode
+        /// </summary>
+        ExecutableFile = 0x81ED,
+
+        /// <summary>
+        ///   120000 file mode
+        /// </summary>
+        SymbolicLink = 0xA000,
+
+        /// <summary>
+        ///   160000 file mode
+        /// </summary>
+        GitLink = 0xE000
+    }
+}
