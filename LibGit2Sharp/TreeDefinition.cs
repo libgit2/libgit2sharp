77fd1e48d22b99dbcac789ae014edaf9328776c2
nulltoken <emeric.fermas@gmail.com>
2012-04-19 10:14:13 +0200

    Add TreeDefinition and TreeEntryDefinition types

---
+++ LibGit2Sharp/TreeDefinition.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using LibGit2Sharp.Core;
+using LibGit2Sharp.Core.Compat;
+
+namespace LibGit2Sharp
+{
+    /// <summary>
+    ///   Holds the meta data of a <see cref = "Tree" />.
+    /// </summary>
+    public class TreeDefinition
+    {
+        private readonly Dictionary<string, TreeEntryDefinition> entries = new Dictionary<string, TreeEntryDefinition>();
+        private readonly Dictionary<string, TreeDefinition> unwrappedTrees = new Dictionary<string, TreeDefinition>();
+
+        /// <summary>
+        ///   Builds a <see cref = "TreeDefinition" /> from an existing <see cref = "Tree" />.
+        /// </summary>
+        /// <param name = "tree">The <see cref = "Tree" /> to be processed.</param>
+        /// <returns>A new <see cref = "TreeDefinition" /> holding the meta data of the <paramref name = "tree" />.</returns>
+        public static TreeDefinition From(Tree tree)
+        {
+            Ensure.ArgumentNotNull(tree, "tree");
+
+            var td = new TreeDefinition();
+
+            foreach (TreeEntry treeEntry in tree)
+            {
+                td.AddEntry(treeEntry.Name, TreeEntryDefinition.From(treeEntry));
+            }
+
+            return td;
+        }
+
+        private void AddEntry(string targetTreeEntryName, TreeEntryDefinition treeEntryDefinition)
+        {
+            if (entries.ContainsKey(targetTreeEntryName))
+            {
+                WrapTree(targetTreeEntryName, treeEntryDefinition);
+                return;
+            }
+
+            entries.Add(targetTreeEntryName, treeEntryDefinition);
+        }
+
+        /// <summary>
+        ///   Adds or replaces a <see cref="TreeEntryDefinition"/> at the specified <paramref name="targetTreeEntryPath"/> location.
+        /// </summary>
+        /// <param name="targetTreeEntryPath">The path within this <see cref="TreeDefinition"/>.</param>
+        /// <param name="treeEntryDefinition">The <see cref="TreeEntryDefinition"/> to be stored at the described location.</param>
+        /// <returns>The current <see cref="TreeDefinition"/>.</returns>
+        public TreeDefinition Add(string targetTreeEntryPath, TreeEntryDefinition treeEntryDefinition)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(targetTreeEntryPath, "targetTreeEntryPath");
+            Ensure.ArgumentNotNull(treeEntryDefinition, "treeEntryDefinition");
+
+            if (Path.IsPathRooted(targetTreeEntryPath))
+            {
+                throw new ArgumentException("The provided path is an absolute path.");
+            }
+
+            if (treeEntryDefinition is TransientTreeTreeEntryDefinition)
+            {
+                throw new InvalidOperationException(string.Format("The {0} references a target which hasn't been created in the {1} yet. This situation can occur when the target is a whole new {2} being created, or when an existing {2} is being updated because some of its children were added/removed.", typeof(TreeEntryDefinition).Name, typeof(ObjectDatabase).Name, typeof(Tree).Name));
+            }
+
+            Tuple<string, string> segments = ExtractPosixLeadingSegment(targetTreeEntryPath);
+
+            if (segments.Item2 != null)
+            {
+                TreeDefinition td = RetrieveOrBuildTreeDefinition(segments.Item1, true);
+                td.Add(segments.Item2, treeEntryDefinition);
+            }
+            else
+            {
+                AddEntry(segments.Item1, treeEntryDefinition);
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        ///   Adds or replaces a <see cref="TreeEntryDefinition"/>, dynamically built from the provided <see cref="Blob"/>, at the specified <paramref name="targetTreeEntryPath"/> location.
+        /// </summary>
+        /// <param name="targetTreeEntryPath">The path within this <see cref="TreeDefinition"/>.</param>
+        /// <param name="blob">The <see cref="Blob"/> to be stored at the described location.</param>
+        /// <param name="mode">The file related <see cref="Mode"/> attributes.</param>
+        /// <returns>The current <see cref="TreeDefinition"/>.</returns>
+        public TreeDefinition Add(string targetTreeEntryPath, Blob blob, Mode mode)
+        {
+            Ensure.ArgumentNotNull(blob, "blob");
+            Ensure.ArgumentConformsTo(mode,
+                                      m => m.HasAny(new[] { Mode.ExecutableFile, Mode.NonExecutableFile, Mode.NonExecutableGroupWriteableFile }), "mode");
+
+            TreeEntryDefinition ted = TreeEntryDefinition.From(blob, mode);
+
+            return Add(targetTreeEntryPath, ted);
+        }
+
+        /// <summary>
+        ///   Adds or replaces a <see cref="TreeEntryDefinition"/>, dynamically built from the provided <see cref="Tree"/>, at the specified <paramref name="targetTreeEntryPath"/> location.
+        /// </summary>
+        /// <param name="targetTreeEntryPath">The path within this <see cref="TreeDefinition"/>.</param>
+        /// <param name="tree">The <see cref="Tree"/> to be stored at the described location.</param>
+        /// <returns>The current <see cref="TreeDefinition"/>.</returns>
+        public TreeDefinition Add(string targetTreeEntryPath, Tree tree)
+        {
+            Ensure.ArgumentNotNull(tree, "tree");
+
+            TreeEntryDefinition ted = TreeEntryDefinition.From(tree);
+
+            return Add(targetTreeEntryPath, ted);
+        }
+
+        private TreeDefinition RetrieveOrBuildTreeDefinition(string treeName, bool shouldOverWrite)
+        {
+            TreeDefinition td;
+
+            if (unwrappedTrees.TryGetValue(treeName, out td))
+            {
+                return td;
+            }
+
+            TreeEntryDefinition treeEntryDefinition;
+            bool hasAnEntryBeenFound = entries.TryGetValue(treeName, out treeEntryDefinition);
+
+            if (hasAnEntryBeenFound)
+            {
+                switch (treeEntryDefinition.Type)
+                {
+                    case GitObjectType.Tree:
+                        td = From(treeEntryDefinition.Target as Tree);
+                        break;
+
+                    case GitObjectType.Blob:
+                        if (shouldOverWrite)
+                        {
+                            td = new TreeDefinition();
+                            break;
+                        }
+
+                        return null;
+
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+            else
+            {
+                if (!shouldOverWrite)
+                {
+                    return null;
+                }
+
+                td = new TreeDefinition();
+            }
+
+            entries[treeName] = new TransientTreeTreeEntryDefinition();
+
+            unwrappedTrees.Add(treeName, td);
+            return td;
+        }
+
+        private void WrapTree(string entryName, TreeEntryDefinition treeEntryDefinition)
+        {
+            entries[entryName] = treeEntryDefinition;
+            unwrappedTrees.Remove(entryName);
+        }
+
+        /// <summary>
+        ///   Retrieves the <see cref="TreeEntryDefinition"/> located the specified <paramref name="treeEntryPath"/> path.
+        /// </summary>
+        /// <param name="treeEntryPath">The path within this <see cref="TreeDefinition"/>.</param>
+        /// <returns>The found <see cref="TreeEntryDefinition"/> if any; null otherwise.</returns>
+        public TreeEntryDefinition this[string treeEntryPath]
+        {
+            get
+            {
+                Ensure.ArgumentNotNullOrEmptyString(treeEntryPath, "treeEntryPath");
+
+                Tuple<string, string> segments = ExtractPosixLeadingSegment(treeEntryPath);
+
+                if (segments.Item2 != null)
+                {
+                    TreeDefinition td = RetrieveOrBuildTreeDefinition(segments.Item1, false);
+                    return td == null ? null : td[segments.Item2];
+                }
+
+                TreeEntryDefinition treeEntryDefinition;
+                return !entries.TryGetValue(segments.Item1, out treeEntryDefinition) ? null : treeEntryDefinition;
+            }
+        }
+
+        private static Tuple<string, string> ExtractPosixLeadingSegment(FilePath targetPath)
+        {
+            string[] segments = targetPath.Posix.Split(new[] { '/' }, 2);
+
+            if (segments[0] == string.Empty || (segments.Length == 2 && (segments[1] == string.Empty || segments[1].StartsWith("/"))))
+            {
+                throw new ArgumentException(string.Format("'{0}' is not a valid path.", targetPath));
+            }
+
+            return new Tuple<string, string>(segments[0], segments.Length == 2 ? segments[1] : null);
+        }
+    }
+}
---
+++ LibGit2Sharp/TreeEntryDefinition.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using LibGit2Sharp.Core;
+using LibGit2Sharp.Core.Compat;
+
+namespace LibGit2Sharp
+{
+    /// <summary>
+    ///   Holds the meta data of a <see cref = "TreeEntry" />.
+    /// </summary>
+    public class TreeEntryDefinition
+    {
+        private Lazy<GitObject> target;
+
+        private static readonly LambdaEqualityHelper<TreeEntryDefinition> equalityHelper =
+            new LambdaEqualityHelper<TreeEntryDefinition>(new Func<TreeEntryDefinition, object>[] { x => x.Mode, x => x.Type, x => x.TargetId });
+
+        internal TreeEntryDefinition()
+        {
+        }
+
+        /// <summary>
+        ///   Gets file mode.
+        /// </summary>
+        public virtual Mode Mode { get; private set; }
+
+        /// <summary>
+        ///   Gets the <see cref = "GitObjectType" /> of the target being pointed at.
+        /// </summary>
+        public virtual GitObjectType Type { get; private set; }
+
+        /// <summary>
+        ///   Gets the <see cref = "ObjectId" /> of the target being pointed at.
+        /// </summary>
+        public virtual ObjectId TargetId { get; private set; }
+
+        internal virtual GitObject Target
+        {
+            get { return target.Value; }
+        }
+
+        internal static TreeEntryDefinition From(TreeEntry treeEntry)
+        {
+            return new TreeEntryDefinition
+                       {
+                           Mode = treeEntry.Mode,
+                           Type = treeEntry.Type,
+                           TargetId = treeEntry.TargetId,
+                           target = new Lazy<GitObject>(() => treeEntry.Target)
+                       };
+        }
+
+        internal static TreeEntryDefinition From(Blob blob, Mode mode)
+        {
+            return new TreeEntryDefinition
+                       {
+                           Mode = mode,
+                           Type = GitObjectType.Blob,
+                           TargetId = blob.Id,
+                           target = new Lazy<GitObject>(() => blob)
+                       };
+        }
+
+        internal static TreeEntryDefinition From(Tree tree)
+        {
+            return new TreeEntryDefinition
+                       {
+                           Mode = Mode.Directory,
+                           Type = GitObjectType.Tree,
+                           TargetId = tree.Id,
+                           target = new Lazy<GitObject>(() => tree)
+                       };
+        }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref = "Object" /> is equal to the current <see cref = "TreeEntryDefinition" />.
+        /// </summary>
+        /// <param name = "obj">The <see cref = "Object" /> to compare with the current <see cref = "TreeEntryDefinition" />.</param>
+        /// <returns>True if the specified <see cref = "Object" /> is equal to the current <see cref = "TreeEntryDefinition" />; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TreeEntryDefinition);
+        }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref = "TreeEntryDefinition" /> is equal to the current <see cref = "TreeEntryDefinition" />.
+        /// </summary>
+        /// <param name = "other">The <see cref = "TreeEntryDefinition" /> to compare with the current <see cref = "TreeEntryDefinition" />.</param>
+        /// <returns>True if the specified <see cref = "TreeEntryDefinition" /> is equal to the current <see cref = "TreeEntryDefinition" />; otherwise, false.</returns>
+        public bool Equals(TreeEntryDefinition other)
+        {
+            return equalityHelper.Equals(this, other);
+        }
+
+        /// <summary>
+        ///   Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return equalityHelper.GetHashCode(this);
+        }
+
+        /// <summary>
+        ///   Tests if two <see cref = "TreeEntryDefinition" /> are equal.
+        /// </summary>
+        /// <param name = "left">First <see cref = "TreeEntryDefinition" /> to compare.</param>
+        /// <param name = "right">Second <see cref = "TreeEntryDefinition" /> to compare.</param>
+        /// <returns>True if the two objects are equal; false otherwise.</returns>
+        public static bool operator ==(TreeEntryDefinition left, TreeEntryDefinition right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        ///   Tests if two <see cref = "TreeEntryDefinition" /> are different.
+        /// </summary>
+        /// <param name = "left">First <see cref = "TreeEntryDefinition" /> to compare.</param>
+        /// <param name = "right">Second <see cref = "TreeEntryDefinition" /> to compare.</param>
+        /// <returns>True if the two objects are different; false otherwise.</returns>
+        public static bool operator !=(TreeEntryDefinition left, TreeEntryDefinition right)
+        {
+            return !Equals(left, right);
+        }
+    }
+
+    internal abstract class TransientTreeEntryDefinition : TreeEntryDefinition
+    {
+        public override ObjectId TargetId
+        {
+            get { return ObjectId.Zero; }
+        }
+
+        internal override GitObject Target
+        {
+            get { return null; }
+        }
+    }
+
+    internal class TransientTreeTreeEntryDefinition : TransientTreeEntryDefinition
+    {
+        public override Mode Mode
+        {
+            get { return Mode.Directory; }
+        }
+
+        public override GitObjectType Type
+        {
+            get { return GitObjectType.Tree; }
+        }
+    }
+}
