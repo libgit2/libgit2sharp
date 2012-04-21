1658e84d428824aae8f62e8be5e70c4664e2663d
nulltoken <emeric.fermas@gmail.com>
2012-04-21 21:38:59 +0200

    Improve documentation related to the creation of Commits

--- LibGit2Sharp/CommitCollection.cs
+++ LibGit2Sharp/CommitCollection.cs
@@ -126,11 +126,13 @@ namespace LibGit2Sharp
 
         /// <summary>
         ///   Stores the content of the <see cref = "Repository.Index" /> as a new <see cref = "Commit" /> into the repository.
+        ///   The tip of the <see cref = "Repository.Head"/> will be used as the parent of this new Commit.
+        ///   Once the commit is created, the <see cref = "Repository.Head"/> will move forward to point at it.
         /// </summary>
         /// <param name = "message">The description of why a change was made to the repository.</param>
         /// <param name = "author">The <see cref = "Signature" /> of who made the change.</param>
         /// <param name = "committer">The <see cref = "Signature" /> of who added the change to the repository.</param>
-        /// <param name="amendPreviousCommit">True to amend the current <see cref="Commit"/> pointed at by <see cref="Repository.Head"/>, false otherwise.</param>
+        /// <param name = "amendPreviousCommit">True to amend the current <see cref = "Commit"/> pointed at by <see cref = "Repository.Head"/>, false otherwise.</param>
         /// <returns>The generated <see cref = "Commit" />.</returns>
         public Commit Create(string message, Signature author, Signature committer, bool amendPreviousCommit)
         {
