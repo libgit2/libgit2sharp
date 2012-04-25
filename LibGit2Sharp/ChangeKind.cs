76d41a923376c6a487dc796d180249d41291eea7
yorah <yoram.harmelin@gmail.com>
2012-04-25 18:26:59 +0200

    Add tree to tree Diff feature

---
+++ LibGit2Sharp/ChangeKind.cs
@@ -0,0 +1,48 @@
+namespace LibGit2Sharp
+{
+    /// <summary>
+    ///   The kind of changes that a Diff can report.
+    /// </summary>
+    public enum ChangeKind
+    {
+        /// <summary>
+        ///   No changes detected.
+        /// </summary>
+        Unmodified = 0,
+
+        /// <summary>
+        ///   The file was added.
+        /// </summary>
+        Added = 1,
+
+        /// <summary>
+        ///   The file was deleted.
+        /// </summary>
+        Deleted = 2,
+
+        /// <summary>
+        ///   The file content was modified.
+        /// </summary>
+        Modified = 3,
+
+        /// <summary>
+        ///   The file was renamed.
+        /// </summary>
+        Renamed = 4,
+
+        /// <summary>
+        ///   The file was copied.
+        /// </summary>
+        Copied = 5,
+
+        /// <summary>
+        ///   The file is ignored in the workdir.
+        /// </summary>
+        Ignored = 6,
+
+        /// <summary>
+        ///   The file is untracked in the workdir.
+        /// </summary>
+        Untracked = 7,
+    }
+}
