65aa0aa9737617b694558b92175387019643dac2
Keith Dahlby <dahlbyk@gmail.com>
2012-05-21 23:07:14 -0500

    Add Leaks build configuration for #141

--- LibGit2Sharp/Core/Handles/SafeHandleBase.cs
+++ LibGit2Sharp/Core/Handles/SafeHandleBase.cs
@@ -1,13 +1,36 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace LibGit2Sharp.Core.Handles
 {
     internal abstract class SafeHandleBase : SafeHandle
     {
+#if LEAKS
+        private readonly string trace;
+#endif
+
         protected SafeHandleBase()
             : base(IntPtr.Zero, true)
         {
+#if LEAKS
+            trace = new StackTrace(2, true).ToString();
+#endif
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+#if DEBUG
+            if (!disposing && !IsInvalid)
+            {
+                Trace.WriteLine(string.Format("A {0} handle wrapper has not been properly disposed.", GetType().Name));
+#if LEAKS
+                Trace.WriteLine(trace);
+#endif
+                Trace.WriteLine("");
+            }
+#endif
+            base.Dispose(disposing);
         }
 
         public override bool IsInvalid
