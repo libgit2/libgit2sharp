13745b7423d25e2754034ec764c3cf3bb9021f61
nulltoken <emeric.fermas@gmail.com>
2012-05-28 09:14:25 +0200

    Build libgit2 as part of the mono build

--- build.libgit2sharp.sh
+++ build.libgit2sharp.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+PREVIOUS_LD=$LD_LIBRARY_PATH
+
+mkdir cmake-build && cd cmake-build
+
+cmake -DBUILD_SHARED_LIBS:BOOL=ON -DTHREADSAFE:BOOL=ON -DBUILD_CLAR:BOOL=OFF -DCMAKE_INSTALL_PREFIX=./libgit2-bin ../libgit2
+cmake --build . --target install
+
+LD_LIBRARY_PATH=$PWD/libgit2-bin/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+
+cd ..
+
+echo $LD_LIBRARY_PATH
 xbuild CI-build.msbuild /t:Deploy
+
+LD_LIBRARY_PATH=$PREVIOUS_LD
+export LD_LIBRARY_PATH
