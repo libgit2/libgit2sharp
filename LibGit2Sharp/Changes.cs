b60f21bf5f3a431d924a91c99fc80459e5c18e83
Keith Dahlby <dahlbyk@gmail.com>
2012-06-08 23:50:55 -0500

    Extract Changes base class from ContentChanges
    TreeEntryChanges inherited from ContentChanges, but they're really not the same thing.

---
+++ LibGit2Sharp/Changes.cs
@@ -0,0 +1,40 @@
+using System.Text;
+
+namespace LibGit2Sharp
+{
+    /// <summary>
+    ///   Base class for changes.
+    /// </summary>
+    public abstract class Changes
+    {
+        private readonly StringBuilder patchBuilder = new StringBuilder();
+
+        /// <summary>
+        ///   The number of lines added.
+        /// </summary>
+        public int LinesAdded { get; internal set; }
+
+        /// <summary>
+        ///   The number of lines deleted.
+        /// </summary>
+        public int LinesDeleted { get; internal set; }
+
+        /// <summary>
+        ///   The patch corresponding to these changes.
+        /// </summary>
+        public string Patch
+        {
+            get { return patchBuilder.ToString(); }
+        }
+
+        internal StringBuilder PatchBuilder
+        {
+            get { return patchBuilder; }
+        }
+
+        /// <summary>
+        ///   Determines if at least one side of the comparison holds binary content.
+        /// </summary>
+        public bool IsBinaryComparison { get; protected set; }
+    }
+}
