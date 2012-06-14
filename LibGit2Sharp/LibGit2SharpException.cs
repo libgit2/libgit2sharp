33f5e8588df59e98f3dddc5a42d2a501b39a51e3
Keith Dahlby <dahlbyk@gmail.com>
2012-06-13 21:44:05 -0500

    Remove obsolete LibGit2Exception

--- LibGit2Sharp/LibGit2SharpException.cs
+++ LibGit2Sharp/LibGit2SharpException.cs
@@ -8,40 +8,6 @@ namespace LibGit2Sharp
     /// <summary>
     ///   The exception that is thrown when an error occurs during application execution.
     /// </summary>
-    [Obsolete("This type will be removed in the next release. Please use LibGit2SharpException instead.")]
-    [Serializable]
-    public class LibGit2Exception : LibGit2SharpException
-    {
-        /// <summary>
-        ///   Initializes a new instance of the <see cref = "LibGit2Exception" /> class.
-        /// </summary>
-        public LibGit2Exception()
-        {
-        }
-
-        /// <summary>
-        ///   Initializes a new instance of the <see cref = "LibGit2Exception" /> class with a specified error message.
-        /// </summary>
-        /// <param name = "message">A message that describes the error. </param>
-        public LibGit2Exception(string message)
-            : base(message)
-        {
-        }
-
-        /// <summary>
-        ///   Initializes a new instance of the <see cref = "LibGit2Exception" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.
-        /// </summary>
-        /// <param name = "message">The error message that explains the reason for the exception. </param>
-        /// <param name = "innerException">The exception that is the cause of the current exception. If the <paramref name = "innerException" /> parameter is not a null reference, the current exception is raised in a catch block that handles the inner exception.</param>
-        public LibGit2Exception(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-    }
-
-    /// <summary>
-    ///   The exception that is thrown when an error occurs during application execution.
-    /// </summary>
     [Serializable]
     public class LibGit2SharpException : Exception
     {
