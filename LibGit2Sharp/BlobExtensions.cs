664fdada9541967af4b011f62e44d13c6ae8182a
Haacked <haacked@gmail.com>
2012-06-17 23:29:16 +0200

    Add BlobExtensions

---
+++ LibGit2Sharp/BlobExtensions.cs
@@ -0,0 +1,30 @@
+﻿﻿using System.Text;
+
+namespace LibGit2Sharp
+{
+    /// <summary>
+    ///   Provides helper overloads to a <see cref = "Blob" />.
+    /// </summary>
+    public static class BlobExtensions
+    {
+        /// <summary>
+        ///   Gets the blob content decoded as UTF-8.
+        /// </summary>
+        /// <param name="blob">The blob for which the content will be returned.</param>
+        /// <returns>Blob content as UTF-8</returns>
+        public static string ContentAsUtf8(this Blob blob)
+        {
+            return Encoding.UTF8.GetString(blob.Content);
+        }
+
+        /// <summary>
+        ///   Gets the blob content decoded as Unicode.
+        /// </summary>
+        /// <param name="blob">The blob for which the content will be returned.</param>
+        /// <returns>Blob content as unicode.</returns>
+        public static string ContentAsUnicode(this Blob blob)
+        {
+            return Encoding.Unicode.GetString(blob.Content);
+        }
+    }
+}
