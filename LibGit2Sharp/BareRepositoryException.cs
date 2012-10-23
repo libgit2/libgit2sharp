0912002c7d7166da54619498ad1df903637a3fda
Jameson Miller <jamill@microsoft.com>
2012-10-23 16:54:13 -0400

    Fix typo in BareRepositoryException

--- LibGit2Sharp/BareRepositoryException.cs
+++ LibGit2Sharp/BareRepositoryException.cs
@@ -38,7 +38,7 @@ namespace LibGit2Sharp
         }
 
         /// <summary>
-        ///   Initializes a new instance of the <see cref = "LibGit2Sharp.AmbiguousException" /> class with a serialized data.
+        ///   Initializes a new instance of the <see cref = "LibGit2Sharp.BareRepositoryException" /> class with a serialized data.
         /// </summary>
         /// <param name = "info">The <see cref="SerializationInfo "/> that holds the serialized object data about the exception being thrown.</param>
         /// <param name = "context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
