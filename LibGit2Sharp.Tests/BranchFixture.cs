a66d8ff7c0820aaacac63a5b2761facd7df7fee3
Jameson Miller <jamill@microsoft.com>
2012-11-05 18:04:29 +0100

    Checkout is not allowed in a bare repository

--- LibGit2Sharp.Tests/BranchFixture.cs
+++ LibGit2Sharp.Tests/BranchFixture.cs
@@ -421,17 +421,17 @@ namespace LibGit2Sharp.Tests
         }
 
         [Theory]
-        [InlineData("test")]
-        [InlineData("refs/heads/test")]
-        public void CanCheckoutAnExistingBranch(string name)
+        [InlineData("i-do-numbers")]
+        [InlineData("diff-test-cases")]
+        public void CanCheckoutAnExistingBranch(string branchName)
         {
-            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo();
+            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo(StandardTestRepoWorkingDirPath);
             using (var repo = new Repository(path.RepositoryPath))
             {
                 Branch master = repo.Branches["master"];
                 Assert.True(master.IsCurrentRepositoryHead);
 
-                Branch branch = repo.Branches[name];
+                Branch branch = repo.Branches[branchName];
                 Assert.NotNull(branch);
 
                 Branch test = repo.Checkout(branch);
@@ -446,17 +446,17 @@ namespace LibGit2Sharp.Tests
         }
 
         [Theory]
-        [InlineData("test")]
-        [InlineData("refs/heads/test")]
-        public void CanCheckoutAnExistingBranchByName(string name)
+        [InlineData("i-do-numbers")]
+        [InlineData("diff-test-cases")]
+        public void CanCheckoutAnExistingBranchByName(string branchName)
         {
-            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo();
+            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo(StandardTestRepoWorkingDirPath);
             using (var repo = new Repository(path.RepositoryPath))
             {
                 Branch master = repo.Branches["master"];
                 Assert.True(master.IsCurrentRepositoryHead);
 
-                Branch test = repo.Checkout(name);
+                Branch test = repo.Checkout(branchName);
                 Assert.False(repo.Info.IsHeadDetached);
 
                 Assert.False(test.IsRemote);
@@ -472,7 +472,7 @@ namespace LibGit2Sharp.Tests
         [InlineData("refs/tags/lw")]
         public void CanCheckoutAnArbitraryCommit(string commitPointer)
         {
-            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo();
+            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo(StandardTestRepoWorkingDirPath);
             using (var repo = new Repository(path.RepositoryPath))
             {
                 Branch master = repo.Branches["master"];
@@ -480,25 +480,35 @@ namespace LibGit2Sharp.Tests
 
                 Branch detachedHead = repo.Checkout(commitPointer);
 
+                Assert.Equal(repo.Head, detachedHead);
+                Assert.Equal(repo.Lookup(commitPointer).Sha, detachedHead.Tip.Sha);
+                Assert.True(repo.Head.IsCurrentRepositoryHead);
                 Assert.True(repo.Info.IsHeadDetached);
 
+                Assert.True(detachedHead.IsCurrentRepositoryHead);
                 Assert.False(detachedHead.IsRemote);
                 Assert.Equal(detachedHead.Name, detachedHead.CanonicalName);
-                Assert.Equal("(no branch)", detachedHead.CanonicalName);
-                Assert.Equal(repo.Lookup(commitPointer).Sha, detachedHead.Tip.Sha);
 
-                Assert.Equal(repo.Head, detachedHead);
+                Assert.Equal("(no branch)", detachedHead.CanonicalName);
 
                 Assert.False(master.IsCurrentRepositoryHead);
-                Assert.True(detachedHead.IsCurrentRepositoryHead);
-                Assert.True(repo.Head.IsCurrentRepositoryHead);
+            }
+        }
+
+        [Fact]
+        public void CheckingOutInABareRepoThrows()
+        {
+            using (var repo = new Repository(BareTestRepoPath))
+            {
+                Assert.Throws<InvalidOperationException>(() => repo.Checkout(repo.Branches["refs/heads/test"]));
+                Assert.Throws<InvalidOperationException>(() => repo.Checkout("refs/heads/test"));
             }
         }
 
         [Fact]
         public void CheckingOutANonExistingBranchThrows()
         {
-            using (var repo = new Repository(BareTestRepoPath))
+            using (var repo = new Repository(StandardTestRepoWorkingDirPath))
             {
                 Assert.Throws<LibGit2SharpException>(() => repo.Checkout("i-do-not-exist"));
             }
@@ -507,7 +517,7 @@ namespace LibGit2Sharp.Tests
         [Fact]
         public void CheckingOutABranchWithBadParamsThrows()
         {
-            using (var repo = new Repository(BareTestRepoPath))
+            using (var repo = new Repository(StandardTestRepoWorkingDirPath))
             {
                 Assert.Throws<ArgumentException>(() => repo.Checkout(string.Empty));
                 Assert.Throws<ArgumentNullException>(() => repo.Checkout(default(Branch)));
