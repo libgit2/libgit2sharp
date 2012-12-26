2511fabcfddf908269a03d2eb83c4cd4c00f6677
nulltoken <emeric.fermas@gmail.com>
2012-12-26 16:00:40 +0100

    Eagerly load the Configuration if any config path is provided
    Fix #277

--- LibGit2Sharp.Tests/DiffTreeToTreeFixture.cs
+++ LibGit2Sharp.Tests/DiffTreeToTreeFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using LibGit2Sharp.Tests.TestHelpers;
@@ -387,6 +388,72 @@ namespace LibGit2Sharp.Tests
 
                 Assert.Equal(0, changes.Count());
             }
+        }
+
+        [Fact]
+        public void ComparingReliesOnProvidedConfigEntriesIfAny()
+        {
+            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo(StandardTestRepoWorkingDirPath);
+
+            const string file = "1/branch_file.txt";
+
+            using (var repo = new Repository(path.DirectoryPath))
+            {
+                TreeEntry entry = repo.Head[file];
+                Assert.Equal(Mode.ExecutableFile, entry.Mode);
+
+                // Recreate the file in the workdir without the executable bit
+                string fullpath = Path.Combine(repo.Info.WorkingDirectory, file);
+                File.Delete(fullpath);
+                File.WriteAllBytes(fullpath, ((Blob)(entry.Target)).Content);
+
+                // Unset the local core.filemode, if any.
+                repo.Config.Unset("core.filemode", ConfigurationLevel.Local);
+            }
+
+            SelfCleaningDirectory scd = BuildSelfCleaningDirectory();
+
+            var options = BuildFakeSystemConfigFilemodeOption(scd, true);
+
+            using (var repo = new Repository(path.DirectoryPath, options))
+            {
+                TreeChanges changes = repo.Diff.Compare(new []{ file });
+
+                Assert.Equal(1, changes.Count());
+
+                var change = changes.Modified.Single();
+                Assert.Equal(Mode.ExecutableFile, change.OldMode);
+                Assert.Equal(Mode.NonExecutableFile, change.Mode);
+            }
+
+            options = BuildFakeSystemConfigFilemodeOption(scd, false);
+
+            using (var repo = new Repository(path.DirectoryPath, options))
+            {
+                TreeChanges changes = repo.Diff.Compare(new[] { file });
+
+                Assert.Equal(0, changes.Count());
+            }
+        }
+
+        private RepositoryOptions BuildFakeSystemConfigFilemodeOption(
+            SelfCleaningDirectory scd,
+            bool value)
+        {
+            Directory.CreateDirectory(scd.DirectoryPath);
+
+            var options = new RepositoryOptions
+                              {
+                                  SystemConfigurationLocation = Path.Combine(
+                                      scd.RootedDirectoryPath, "fake-system.config")
+                              };
+
+            StringBuilder sb = new StringBuilder()
+                .AppendFormat("[core]{0}", Environment.NewLine)
+                .AppendFormat("filemode = {1}{0}", Environment.NewLine, value);
+            File.WriteAllText(options.SystemConfigurationLocation, sb.ToString());
+
+            return options;
         }
     }
 }
