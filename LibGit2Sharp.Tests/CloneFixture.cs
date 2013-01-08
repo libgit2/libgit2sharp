30cd0546510bd4c64057cf10eec2238db5639424
nulltoken <emeric.fermas@gmail.com>
2013-01-08 12:08:42 +0100

    Move clone related tests into a separate fixture

---
+++ LibGit2Sharp.Tests/CloneFixture.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using LibGit2Sharp.Tests.TestHelpers;
+using Xunit;
+using Xunit.Extensions;
+
+namespace LibGit2Sharp.Tests
+{
+    public class CloneFixture : BaseFixture
+    {
+        [Theory]
+        [InlineData("http://github.com/libgit2/TestGitRepository")]
+        [InlineData("https://github.com/libgit2/TestGitRepository")]
+        [InlineData("git://github.com/libgit2/TestGitRepository")]
+        //[InlineData("git@github.com:libgit2/TestGitRepository")]
+        public void CanClone(string url)
+        {
+            var scd = BuildSelfCleaningDirectory();
+            using (Repository repo = Repository.Clone(url, scd.RootedDirectoryPath))
+            {
+                string dir = repo.Info.Path;
+                Assert.True(Path.IsPathRooted(dir));
+                Assert.True(Directory.Exists(dir));
+
+                Assert.NotNull(repo.Info.WorkingDirectory);
+                Assert.Equal(Path.Combine(scd.RootedDirectoryPath, ".git" + Path.DirectorySeparatorChar), repo.Info.Path);
+                Assert.False(repo.Info.IsBare);
+
+                Assert.True(File.Exists(Path.Combine(scd.RootedDirectoryPath, "master.txt")));
+                Assert.Equal(repo.Head.Name, "master");
+                Assert.Equal(repo.Head.Tip.Id.ToString(), "49322bb17d3acc9146f98c97d078513228bbf3c0");
+            }
+        }
+
+        [Theory]
+        [InlineData("http://github.com/libgit2/TestGitRepository")]
+        [InlineData("https://github.com/libgit2/TestGitRepository")]
+        [InlineData("git://github.com/libgit2/TestGitRepository")]
+        //[InlineData("git@github.com:libgit2/TestGitRepository")]
+        public void CanCloneBarely(string url)
+        {
+            var scd = BuildSelfCleaningDirectory();
+            using (Repository repo = Repository.Clone(url, scd.RootedDirectoryPath, bare: true))
+            {
+                string dir = repo.Info.Path;
+                Assert.True(Path.IsPathRooted(dir));
+                Assert.True(Directory.Exists(dir));
+
+                Assert.Null(repo.Info.WorkingDirectory);
+                Assert.Equal(scd.RootedDirectoryPath + Path.DirectorySeparatorChar, repo.Info.Path);
+                Assert.True(repo.Info.IsBare);
+            }
+        }
+
+        [Theory]
+        [InlineData("git://github.com/libgit2/TestGitRepository")]
+        public void WontCheckoutIfAskedNotTo(string url)
+        {
+            var scd = BuildSelfCleaningDirectory();
+            using (Repository repo = Repository.Clone(url, scd.RootedDirectoryPath, checkout: false))
+            {
+                Assert.False(File.Exists(Path.Combine(scd.RootedDirectoryPath, "master.txt")));
+            }
+        }
+
+        [Theory]
+        [InlineData("git://github.com/libgit2/TestGitRepository")]
+        public void CallsProgressCallbacks(string url)
+        {
+            bool transferWasCalled = false;
+            bool checkoutWasCalled = false;
+
+            var scd = BuildSelfCleaningDirectory();
+            using (Repository repo = Repository.Clone(url, scd.RootedDirectoryPath,
+                onTransferProgress: (_) => transferWasCalled = true,
+                onCheckoutProgress: (a, b, c) => checkoutWasCalled = true))
+            {
+                Assert.True(transferWasCalled);
+                Assert.True(checkoutWasCalled);
+            }
+        }
+    }
+}
--- LibGit2Sharp.Tests/RepositoryFixture.cs
+++ LibGit2Sharp.Tests/RepositoryFixture.cs
@@ -4,14 +4,12 @@ using System.IO;
 using System.Linq;
 using LibGit2Sharp.Tests.TestHelpers;
 using Xunit;
-using Xunit.Extensions;
 
 namespace LibGit2Sharp.Tests
 {
     public class RepositoryFixture : BaseFixture
     {
         private const string commitSha = "8496071c1b46c854b31185ea97743be6a8774479";
-        private const string TestRepoUrl = "git://github.com/libgit2/TestGitRepository";
 
         [Fact]
         public void CanCreateBareRepo()
@@ -465,76 +463,6 @@ namespace LibGit2Sharp.Tests
 
                 repo.Refs.Add("HEAD", branchName, true);
                 Assert.False(repo.Info.IsHeadOrphaned);
-            }
-        }
-
-        [Theory]
-        [InlineData("http://github.com/libgit2/TestGitRepository")]
-        [InlineData("https://github.com/libgit2/TestGitRepository")]
-        [InlineData("git://github.com/libgit2/TestGitRepository")]
-        //[InlineData("git@github.com:libgit2/TestGitRepository")]
-        public void CanClone(string url)
-        {
-            var scd = BuildSelfCleaningDirectory();
-            using (Repository repo = Repository.Clone(url, scd.RootedDirectoryPath))
-            {
-                string dir = repo.Info.Path;
-                Assert.True(Path.IsPathRooted(dir));
-                Assert.True(Directory.Exists(dir));
-
-                Assert.NotNull(repo.Info.WorkingDirectory);
-                Assert.Equal(Path.Combine(scd.RootedDirectoryPath, ".git" + Path.DirectorySeparatorChar), repo.Info.Path);
-                Assert.False(repo.Info.IsBare);
-
-                Assert.True(File.Exists(Path.Combine(scd.RootedDirectoryPath, "master.txt")));
-                Assert.Equal(repo.Head.Name, "master");
-                Assert.Equal(repo.Head.Tip.Id.ToString(), "49322bb17d3acc9146f98c97d078513228bbf3c0");
-            }
-        }
-
-        [Theory]
-        [InlineData("http://github.com/libgit2/TestGitRepository")]
-        [InlineData("https://github.com/libgit2/TestGitRepository")]
-        [InlineData("git://github.com/libgit2/TestGitRepository")]
-        //[InlineData("git@github.com:libgit2/TestGitRepository")]
-        public void CanCloneBarely(string url)
-        {
-            var scd = BuildSelfCleaningDirectory();
-            using (Repository repo = Repository.Clone(url, scd.RootedDirectoryPath, bare:true))
-            {
-                string dir = repo.Info.Path;
-                Assert.True(Path.IsPathRooted(dir));
-                Assert.True(Directory.Exists(dir));
-
-                Assert.Null(repo.Info.WorkingDirectory);
-                Assert.Equal(scd.RootedDirectoryPath + Path.DirectorySeparatorChar, repo.Info.Path);
-                Assert.True(repo.Info.IsBare);
-            }
-        }
-
-        [Fact]
-        public void WontCheckoutIfAskedNotTo()
-        {
-            var scd = BuildSelfCleaningDirectory();
-            using (Repository repo = Repository.Clone(TestRepoUrl, scd.RootedDirectoryPath, checkout:false))
-            {
-                Assert.False(File.Exists(Path.Combine(scd.RootedDirectoryPath, "master.txt")));
-            }
-        }
-
-        [Fact]
-        public void CallsProgressCallbacks()
-        {
-            bool transferWasCalled = false;
-            bool checkoutWasCalled = false;
-
-            var scd = BuildSelfCleaningDirectory();
-            using (Repository repo = Repository.Clone(TestRepoUrl, scd.RootedDirectoryPath, 
-                onTransferProgress: (_) => transferWasCalled = true,
-                onCheckoutProgress: (a,b,c) => checkoutWasCalled = true))
-            {
-                Assert.True(transferWasCalled);
-                Assert.True(checkoutWasCalled);
             }
         }
 
