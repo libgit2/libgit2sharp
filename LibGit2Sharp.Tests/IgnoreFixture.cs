dc1848cf8169ccb85f088983115fd8506eae367c
Paul Betts <paul@paulbetts.org>
2013-01-21 15:06:51 -0800

    Add Ignore support
    This PR adds support for the new .gitignore methods in libgit2

---
+++ LibGit2Sharp.Tests/IgnoreFixture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LibGit2Sharp.Tests.TestHelpers;
+using Xunit;
+
+namespace LibGit2Sharp.Tests
+{
+    public class IgnoreFixture : BaseFixture
+    {
+        [Fact]
+        public void TemporaryRulesShouldApplyUntilCleared()
+        {
+            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo(StandardTestRepoWorkingDirPath);
+            using (var repo = new Repository(path.RepositoryPath))
+            {
+                File.WriteAllText(Path.Combine(repo.Info.WorkingDirectory, "Foo.cs"), "Bar");
+
+                Assert.True(repo.Index.RetrieveStatus().Untracked.Contains("Foo.cs"));
+
+                repo.Ignore.AddTemporaryRules(new[] { "*.cs" });
+
+                Assert.False(repo.Index.RetrieveStatus().Untracked.Contains("Foo.cs"));
+
+                repo.Ignore.ResetAllTemporaryRules();
+
+                Assert.True(repo.Index.RetrieveStatus().Untracked.Contains("Foo.cs"));
+            }
+        }
+
+        [Fact]
+        public void IsPathIgnoredShouldVerifyWhetherPathIsIgnored()
+        {
+            TemporaryCloneOfTestRepo path = BuildTemporaryCloneOfTestRepo(StandardTestRepoWorkingDirPath);
+            using (var repo = new Repository(path.RepositoryPath))
+            {
+                File.WriteAllText(Path.Combine(repo.Info.WorkingDirectory, "Foo.cs"), "Bar");
+
+                Assert.False(repo.Ignore.IsPathIgnored("Foo.cs"));
+
+                repo.Ignore.AddTemporaryRules(new[] { "*.cs" });
+
+                Assert.True(repo.Ignore.IsPathIgnored("Foo.cs"));
+
+                repo.Ignore.ResetAllTemporaryRules();
+
+                Assert.False(repo.Ignore.IsPathIgnored("Foo.cs"));
+            }           
+        }
+    }
+}
---
+++ LibGit2Sharp/Ignore.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibGit2Sharp.Core;
+
+namespace LibGit2Sharp
+{
+    public class Ignore
+    {
+        private readonly Repository repo;
+
+        /// <summary>
+        ///   Needed for mocking purposes.
+        /// </summary>
+        protected Ignore()
+        { }
+
+        internal Ignore(Repository repo)
+        {
+            this.repo = repo;
+        }
+
+        /// <summary>
+        ///   Adds a custom .gitignore rule that will be applied to futher operations to the Index. This is in addition
+        ///   to the standard .gitignore rules that would apply as a result of the system/user/repo .gitignore
+        /// </summary>
+        /// <param name="rules">The content of a .gitignore file that will be applied.</param>
+        public virtual void AddTemporaryRules(IEnumerable<string> rules)
+        {
+            var allRules = rules.Aggregate(new StringBuilder(), (acc, x) =>
+            {
+                acc.Append(x);
+                acc.Append("\n");
+                return acc;
+            });
+
+            Proxy.git_ignore_add_rule(repo.Handle, allRules.ToString());
+        }
+
+        /// <summary>
+        ///   Resets all custom rules that were applied via calls to <see cref="Repository.AddCustomIgnoreRules"/> - note that
+        ///   this will not affect the application of the user/repo .gitignores.
+        /// </summary>
+        public virtual void ResetAllTemporaryRules()
+        {
+            Proxy.git_ignore_clear_internal_rules(repo.Handle);
+        }
+
+        /// <summary>
+        ///    Given a relative path, this method determines whether a path should be ignored, checking
+        ///    both the custom ignore rules as well as the "normal" .gitignores.
+        /// </summary>
+        /// <param name="relativePath">A path relative to the repository</param>
+        /// <returns>true if the path should be ignored.</returns>
+        public virtual bool IsPathIgnored(string relativePath)
+        {
+            return Proxy.git_ignore_path_is_ignored(repo.Handle, relativePath);
+        }
+    }
+}
