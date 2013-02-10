2f18086d763d19ce61839bf78b164f1bf605a27e
Andrius Bentkus <andrius.bentkus@gmail.com>
2013-02-10 20:22:00 +0100

    Add RawContentStream.
    Holds the rawcontent objectsafe wrapper.

---
+++ LibGit2Sharp/Core/RawContentStream.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using LibGit2Sharp.Core;
+using LibGit2Sharp.Core.Handles;
+
+namespace LibGit2Sharp.Core
+{
+    internal class RawContentStream : UnmanagedMemoryStream
+    {
+        readonly ObjectSafeWrapper wrapper;
+
+        internal RawContentStream(ObjectId id, RepositorySafeHandle repo,
+            Func<GitObjectSafeHandle, IntPtr> bytePtrProvider, long length)
+            : this(new ObjectSafeWrapper(id, repo), bytePtrProvider, length)
+        {
+        }
+
+        unsafe RawContentStream(ObjectSafeWrapper wrapper,
+            Func<GitObjectSafeHandle, IntPtr> bytePtrProvider, long length)
+            : base((byte *)bytePtrProvider(wrapper.ObjectPtr).ToPointer(), length)
+        {
+            this.wrapper = wrapper;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            wrapper.SafeDispose();
+        }
+    }
+}
+
