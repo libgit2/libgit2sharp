803a595a846fab253f002abb907e1430601c54fa
Keith Dahlby <dahlbyk@gmail.com>
2013-02-13 20:56:13 -0600

    Remove unused usings

--- LibGit2Sharp.Tests/RemoteFixture.cs
+++ LibGit2Sharp.Tests/RemoteFixture.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using LibGit2Sharp.Tests.TestHelpers;
 using Xunit;
 using Xunit.Extensions;
--- LibGit2Sharp/NetworkExtensions.cs
+++ LibGit2Sharp/NetworkExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System.Globalization;
 using LibGit2Sharp.Core;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LibGit2Sharp
 {
