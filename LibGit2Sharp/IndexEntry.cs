a40db856669f0c35a68b5397d0376544119b757a
Keith Dahlby <dahlbyk@gmail.com>
2013-02-13 20:37:17 -0600

    Add StageLevel to IndexEntry.DebuggerDisplay

--- LibGit2Sharp/IndexEntry.cs
+++ LibGit2Sharp/IndexEntry.cs
@@ -134,7 +134,7 @@ namespace LibGit2Sharp
             get
             {
                 return string.Format(CultureInfo.InvariantCulture,
-                    "{0} => \"{1}\"", Path, Id.ToString(7));
+                    "{0} ({1}) => \"{2}\"", Path, StageLevel, Id.ToString(7));
             }
         }
     }
