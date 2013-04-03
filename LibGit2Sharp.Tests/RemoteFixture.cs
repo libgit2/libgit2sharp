d4d314f293c76ab7f41c03eddd3e93d31fa74512
yorah <yoram.harmelin@gmail.com>
2013-04-03 11:41:12 +0200

    Move Fetch tests in separate file

--- LibGit2Sharp.Tests/RemoteFixture.cs
+++ LibGit2Sharp.Tests/RemoteFixture.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using LibGit2Sharp.Tests.TestHelpers;
+﻿using LibGit2Sharp.Tests.TestHelpers;
 using Xunit;
 using Xunit.Extensions;
 
@@ -65,106 +64,6 @@ namespace LibGit2Sharp.Tests
                 Assert.Equal(createdRemote, loadedRemote);
 
                 Assert.NotEqual(oneOrigin, loadedRemote);
-            }
-        }
-
-        [Theory]
-        [InlineData("http://github.com/libgit2/TestGitRepository")]
-        [InlineData("https://github.com/libgit2/TestGitRepository")]
-        [InlineData("git://github.com/libgit2/TestGitRepository.git")]
-        public void CanFetchIntoAnEmptyRepository(string url)
-        {
-            string remoteName = "testRemote";
-
-            var scd = BuildSelfCleaningDirectory();
-            using (var repo = Repository.Init(scd.RootedDirectoryPath))
-            {
-                Remote remote = repo.Network.Remotes.Add(remoteName, url);
-
-                // Set up structures for the expected results
-                // and verifying the RemoteUpdateTips callback.
-                TestRemoteInfo expectedResults = TestRemoteInfo.TestRemoteInstance;
-                ExpectedFetchState expectedFetchState = new ExpectedFetchState(remoteName);
-
-                // Add expected branch objects
-                foreach (KeyValuePair<string, ObjectId> kvp in expectedResults.BranchTips)
-                {
-                    expectedFetchState.AddExpectedBranch(kvp.Key, ObjectId.Zero, kvp.Value);
-                }
-
-                // Add the expected tags
-                string[] expectedTagNames = { "blob", "commit_tree", "annotated_tag" };
-                foreach (string tagName in expectedTagNames)
-                {
-                    TestRemoteInfo.ExpectedTagInfo expectedTagInfo = expectedResults.Tags[tagName];
-                    expectedFetchState.AddExpectedTag(tagName, ObjectId.Zero, expectedTagInfo);
-                }
-
-                // Perform the actual fetch
-                remote.Fetch(onUpdateTips: expectedFetchState.RemoteUpdateTipsHandler);
-
-                // Verify the expected
-                expectedFetchState.CheckUpdatedReferences(repo);
-            }
-        }
-
-        [SkippableFact]
-        public void CanFetchIntoAnEmptyRepositoryWithCredentials()
-        {
-            InconclusiveIf(() => string.IsNullOrEmpty(Constants.PrivateRepoUrl),
-                "Populate Constants.PrivateRepo* to run this test");
-
-            string remoteName = "testRemote";
-
-            var scd = BuildSelfCleaningDirectory();
-            using (var repo = Repository.Init(scd.RootedDirectoryPath))
-            {
-                Remote remote = repo.Network.Remotes.Add(remoteName, Constants.PrivateRepoUrl);
-
-                // Perform the actual fetch
-                remote.Fetch(credentials: new Credentials
-                                              {
-                                                  Username = Constants.PrivateRepoUsername,
-                                                  Password = Constants.PrivateRepoPassword
-                                              });
-            }
-        }
-
-        [Theory]
-        [InlineData("http://github.com/libgit2/TestGitRepository")]
-        [InlineData("https://github.com/libgit2/TestGitRepository")]
-        [InlineData("git://github.com/libgit2/TestGitRepository.git")]
-        public void CanFetchAllTagsIntoAnEmptyRepository(string url)
-        {
-            string remoteName = "testRemote";
-
-            var scd = BuildSelfCleaningDirectory();
-            using (var repo = Repository.Init(scd.RootedDirectoryPath))
-            {
-                Remote remote = repo.Network.Remotes.Add(remoteName, url);
-
-                // Set up structures for the expected results
-                // and verifying the RemoteUpdateTips callback.
-                TestRemoteInfo remoteInfo = TestRemoteInfo.TestRemoteInstance;
-                ExpectedFetchState expectedFetchState = new ExpectedFetchState(remoteName);
-
-                // Add expected branch objects
-                foreach (KeyValuePair<string, ObjectId> kvp in remoteInfo.BranchTips)
-                {
-                    expectedFetchState.AddExpectedBranch(kvp.Key, ObjectId.Zero, kvp.Value);
-                }
-
-                // Add expected tags
-                foreach (KeyValuePair<string, TestRemoteInfo.ExpectedTagInfo> kvp in remoteInfo.Tags)
-                {
-                    expectedFetchState.AddExpectedTag(kvp.Key, ObjectId.Zero, kvp.Value);
-                }
-
-                // Perform the actual fetch
-                remote.Fetch(TagFetchMode.All, onUpdateTips: expectedFetchState.RemoteUpdateTipsHandler);
-
-                // Verify the expected
-                expectedFetchState.CheckUpdatedReferences(repo);
             }
         }
 
