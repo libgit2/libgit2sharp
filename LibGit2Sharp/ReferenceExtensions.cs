1465bf31cdfcf59bceab42bb0e8d0543b02503dc
Saaman <romain.magny@gmail.com>
2013-04-26 17:21:35 +0200

    Add extension methods to determine reference kind

--- LibGit2Sharp/ReferenceExtensions.cs
+++ LibGit2Sharp/ReferenceExtensions.cs
@@ -31,5 +31,45 @@ namespace LibGit2Sharp
         {
             return input.StartsWith(prefix, StringComparison.Ordinal);
         }
+
+        /// <summary>
+        ///   Determine if the current <see cref="Reference"/> is a local branch.
+        /// </summary>
+        /// <param name="reference">The <see cref="Reference"/> to test.</param>
+        /// <returns>true if the current <see cref="Reference"/> is a local branch, false otherwise.</returns>
+        public static bool IsLocalBranch(this Reference reference)
+        {
+            return reference.CanonicalName.LooksLikeLocalBranch();
+        }
+
+        /// <summary>
+        ///   Determine if the current <see cref="Reference"/> is a remote tracking branch.
+        /// </summary>
+        /// <param name="reference">The <see cref="Reference"/> to test.</param>
+        /// <returns>true if the current <see cref="Reference"/> is a remote tracking branch, false otherwise.</returns>
+        public static bool IsRemoteTrackingBranch(this Reference reference)
+        {
+            return reference.CanonicalName.LooksLikeRemoteTrackingBranch();
+        }
+
+        /// <summary>
+        ///   Determine if the current <see cref="Reference"/> is a tag.
+        /// </summary>
+        /// <param name="reference">The <see cref="Reference"/> to test.</param>
+        /// <returns>true if the current <see cref="Reference"/> is a tag, false otherwise.</returns>
+        public static bool IsTag(this Reference reference)
+        {
+            return reference.CanonicalName.LooksLikeTag();
+        }
+
+        /// <summary>
+        ///   Determine if the current <see cref="Reference"/> is a note.
+        /// </summary>
+        /// <param name="reference">The <see cref="Reference"/> to test.</param>
+        /// <returns>true if the current <see cref="Reference"/> is a note, false otherwise.</returns>
+        public static bool IsNote(this Reference reference)
+        {
+            return reference.CanonicalName.LooksLikeNote();
+        }
     }
 }
