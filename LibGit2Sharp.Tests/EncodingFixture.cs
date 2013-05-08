36aa3bb44a4fb092a06159681fe8babaf1b66da7
Martin Woodward <martinwo@microsoft.com>
2013-05-08 20:28:19 +0200

    Test case to repro libgit2/libgit2sharp#387
    When invalid encoding is in the author name or other commit metadata,
    libgit2 always returns the text as if it where UTF8 encoded (which it
    should be). jGit will attempt to parse using the current systems
    default encoder. Core git will simply return the bytes leaving it
    up to the client shell to interpret (which posh-git does by
    defaulting to cp-1252 but Git Bash in msysgit shows differently)

---
+++ LibGit2Sharp.Tests/EncodingFixture.cs
@@ -0,0 +1,41 @@
+﻿using LibGit2Sharp.Tests.TestHelpers;
+using System;
+using System.IO;
+using Xunit;
+
+namespace LibGit2Sharp.Tests
+{
+    public class EncodingFixture : BaseFixture
+    {
+        [Fact]
+        public void CorruptAuthorNameIssue387()
+        {
+            string name = "Märtin Woodwärd";
+
+            // Get byte array for UTF8 version of author name.
+            byte[] authorNameBytes = System.Text.Encoding.UTF8.GetBytes(name);
+            
+            var path = CloneStandardTestRepo();
+            using (var repo = new Repository(path))
+            {
+                string newFile = "enctest.txt";
+                
+                Touch(path,newFile,"Some content here");
+                
+                repo.Index.Stage(newFile);
+                
+                // Create an author name as if UTF8 bytes were CP-1252
+                Commit commit = repo.Commit("Commit from lg2#",
+                  new Signature(
+                    System.Text.Encoding.GetEncoding(1252)
+                      .GetString(authorNameBytes),
+                    "martinwo@microsoft.com",
+                    DateTimeOffset.Now));
+
+                Assert.Equal(name, commit.Author.Name);
+            }
+
+        }
+
+    }
+}
