94aeb13043c31246348ecb399639c7779410d807
Ben Straub <bs@github.com>
2013-11-10 11:49:22 -0800

    Fix building with Mono on OS X

--- build.libgit2sharp.sh
+++ build.libgit2sharp.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-PREVIOUS_LD=$LD_LIBRARY_PATH
-
 LIBGIT2SHA=`cat ./LibGit2Sharp/libgit2_hash.txt`
 SHORTSHA=${LIBGIT2SHA:0:7}
 
-rm -rf cmake-build
-mkdir cmake-build && cd cmake-build
+rm -rf libgit2/build
+mkdir libgit2/build
+pushd libgit2/build
+export _BINPATH=`pwd`
 
-cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DTHREADSAFE:BOOL=ON -DBUILD_CLAR:BOOL=OFF -DCMAKE_INSTALL_PREFIX=./libgit2-bin -DLIBGIT2_FILENAME=git2-$SHORTSHA ../libgit2
-cmake --build . --target install
+cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
+      -DTHREADSAFE:BOOL=ON \
+      -DBUILD_CLAR:BOOL=OFF \
+      -DLIBGIT2_FILENAME=git2-$SHORTSHA \
+      -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" \
+      ..
+cmake --build .
 
-LD_LIBRARY_PATH=$PWD/libgit2-bin/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$_BINPATH:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$_BINPATH:$DYLD_LIBRARY_PATH
 
-cd ..
+popd
 
+echo $DYLD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 xbuild CI-build.msbuild /t:Deploy
 
-EXIT_CODE=$?
-
-LD_LIBRARY_PATH=$PREVIOUS_LD
-export LD_LIBRARY_PATH
-
-exit $EXIT_CODE
+exit $?
