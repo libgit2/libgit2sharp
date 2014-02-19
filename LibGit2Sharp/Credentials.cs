19f83bc0fe893920bf4b884259cb9f74b47b0d2d
Daniel Brown <DanTonyBrown@Hotmail.co.uk>
2014-02-19 02:06:31 +0000

    Added Constructor which adds username and password

--- LibGit2Sharp/Credentials.cs
+++ LibGit2Sharp/Credentials.cs
@@ -5,6 +5,12 @@
     /// </summary>
     public sealed class Credentials
     {
+        public Credentials();
+        public Credentials(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
         /// <summary>
         /// Username for username/password authentication (as in HTTP basic auth).
         /// </summary>
