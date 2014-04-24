17fee2b51e2350821e21554a22275ef69c0e30be
nulltoken <emeric.fermas@gmail.com>
2014-04-24 20:11:15 +0200

    Make Travis build target osx platform

---
+++ CI/travis.linux.install.deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ev
+
+sudo apt-get install cmake mono-devel mono-gmcs
---
+++ CI/travis.osx.install.deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+
+MONO_VER=2.10.10
+
+brew update
+brew install cmake
+
+wget "http://download.mono-project.com/archive/${MONO_VER}/macos-10-x86/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
+hdid "MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.dmg"
+sudo installer -pkg "/Volumes/Mono Framework MDK ${MONO_VER}/MonoFramework-MDK-${MONO_VER}.macos10.xamarin.x86.pkg" -target /
