b67edde8cd671f7555d7f8c03bfbc3f07cb5a510
Ian Clanton-Thuon <ianc@microsoft.com>
2014-09-01 16:09:08 -0700

    Including basic tests for ConfigurationEntry.

---
+++ LibGit2Sharp.Tests/ConfigurationEntryFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LibGit2Sharp.Tests.TestHelpers;
+using Xunit;
+
+namespace LibGit2Sharp.Tests
+{
+    public class ConfigurationEntryFixture : BaseFixture
+    {
+        private const string testKey = "testKey";
+        private const string testStringValue = "testValue";
+        private const int testIntValue = 1209812;
+        private const ConfigurationLevel testLevel = ConfigurationLevel.Local;
+
+        private static ConfigurationEntry<T> GetTestConfigurationEntry<T>()
+        {
+            if (typeof(T) == typeof(string))
+            {
+                return new ConfigurationEntry<string>(testKey, testStringValue, testLevel) as ConfigurationEntry<T>;
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                return new ConfigurationEntry<int>(testKey, testIntValue, testLevel) as ConfigurationEntry<T>;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [Fact]
+        public void SetsProperties()
+        {
+            ConfigurationEntry<string> configurationEntry = ConfigurationEntryFixture.GetTestConfigurationEntry<string>();
+            Assert.Equal<string>(testKey, configurationEntry.Key);
+            Assert.Equal<string>(testStringValue, configurationEntry.Value);
+            Assert.Equal<ConfigurationLevel>(testLevel, configurationEntry.Level);
+        }
+
+        [Fact]
+        public void SetsPropertiesForPrimitiveType()
+        {
+            ConfigurationEntry<int> configurationEntry = ConfigurationEntryFixture.GetTestConfigurationEntry<int>();
+            Assert.Equal<string>(testKey, configurationEntry.Key);
+            Assert.Equal<int>(testIntValue, configurationEntry.Value);
+            Assert.Equal<ConfigurationLevel>(testLevel, configurationEntry.Level);
+        }
+    }
+}
