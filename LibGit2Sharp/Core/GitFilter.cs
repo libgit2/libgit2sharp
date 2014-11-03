56b379354c0db6144138e49854700205208bb30a
Amy Palamountain <amy@palamounta.in>
2014-11-03 13:09:51 +1300

    Add delegate for shutdown

--- LibGit2Sharp/Core/GitFilter.cs
+++ LibGit2Sharp/Core/GitFilter.cs
@@ -7,7 +7,7 @@ namespace LibGit2Sharp.Core
     /// A git filter
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    internal struct GitFilter
+    internal class GitFilter
     {
         public uint version;
 
@@ -15,12 +15,26 @@ namespace LibGit2Sharp.Core
 
         public IntPtr init;
 
-        public IntPtr shutdown;
+        public git_filter_shutdown_fn shutdown;
         
         public IntPtr check;
 
         public IntPtr apply;
 
         public IntPtr cleanup;
+
+        /* The libgit2 structure definition ends here. Subsequent fields are for libgit2sharp bookkeeping. */
+
+        /// <summary>
+        ///  Shutdown callback on filter
+        /// 
+        /// Specified as `filter.shutdown`, this is an optional callback invoked
+        /// when the filter is unregistered or when libgit2 is shutting down.  It
+        /// will be called once at most and should release resources as needed.
+        /// This may be called even if the `initialize` callback was not made.
+        /// Typically this function will free the `git_filter` object itself.
+        /// </summary>
+        public delegate void git_filter_shutdown_fn(IntPtr filter);
+
     }
 }
