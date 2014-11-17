520988f053ca44c25391dd7b42ec3464b4ae455a
Amy Palamountain <amy@palamounta.in>
2014-11-17 19:41:31 +1300

    Moved to a new file

--- LibGit2Sharp/Filter.cs
+++ LibGit2Sharp/Filter.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Text;
 using LibGit2Sharp.Core;
 using LibGit2Sharp.Core.Handles;
@@ -235,62 +234,6 @@ namespace LibGit2Sharp
         public static bool operator !=(Filter left, Filter right)
         {
             return !Equals(left, right);
-        }
-    }
-
-    public class FilterSource
-    {
-        /// <summary>
-        /// Needed for mocking purposes
-        /// </summary>
-        protected FilterSource() {  }
-
-        internal FilterSource(FilePath path, FilterMode mode)
-        {
-            SourceMode = mode;
-            Path = GetPathSafely(path);
-        }
-
-        /// <summary>
-        /// Take an unmanaged pointer and convert it to filter source callback paramater
-        /// </summary>
-        /// <param name="ptr"></param>
-        /// <returns></returns>
-        public static FilterSource FromNativePtr(IntPtr ptr)
-        {
-            var source = ptr.MarshalAs<GitFilterSource>();
-            FilePath path = LaxFilePathMarshaler.FromNative(source.path) ?? FilePath.Empty;
-            FilterMode gitFilterSourceMode = Proxy.git_filter_source_mode(ptr);
-            return new FilterSource(path, gitFilterSourceMode);
-        }
-
-        /// <summary>
-        /// The filter mode for current file being filtered
-        /// </summary>
-        public virtual FilterMode SourceMode { get; private set; }
-
-        /// <summary>
-        /// The relative path to the file
-        /// </summary>
-        public virtual string Path { get; private set; }
-
-        /// <summary>
-        /// When cleaning and smudging, relative and absolute paths are returned.
-        /// Try and just return the relative path if we can. 
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        string GetPathSafely(FilePath path)
-        {
-            try
-            {
-                var fileInfo = new FileInfo(path.Native);
-                return fileInfo.Name;
-            }
-            catch (Exception)
-            {
-                return path.Native;;
-            }
         }
     }
 
