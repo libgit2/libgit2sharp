be15e9c05def98482f2f95ca3d4f14a7e96f24d5
Richard Mitchell <richard.mitchell@red-gate.com>
2014-11-18 10:45:02 +0000

    Fixup net35, moq via nuget

---
+++ LibGit2Sharp/Net35.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace LibGit2Sharp
+{
+    public class Tuple<T, TS>
+    {
+        public T Item1 { get; private set; }
+        public TS Item2 { get; private set; }
+
+        public Tuple(T item1, TS item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+
+    public static class EnumExt
+    {
+        /// <summary>
+        /// Check to see if a flags enumeration has a specific flag set.
+        /// </summary>
+        /// <param name="variable">Flags enumeration to check</param>
+        /// <param name="value">Flag to check for</param>
+        /// <returns></returns>
+        public static bool HasFlag(this Enum variable, Enum value)
+        {
+            if (variable == null)
+                return false;
+
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            // Not as good as the .NET 4 version of this function, but should be good enough
+            if (!Enum.IsDefined(variable.GetType(), value))
+            {
+                throw new ArgumentException(string.Format(
+                    "Enumeration type mismatch.  The flag is of type '{0}', was expecting '{1}'.",
+                    value.GetType(), variable.GetType()));
+            }
+
+            ulong num = Convert.ToUInt64(value);
+            return ((Convert.ToUInt64(variable) & num) == num);
+
+        }
+
+    }
+
+    /// <summary>
+    /// Provides support for lazy initialization.
+    /// </summary>
+    /// <typeparam name="T">Specifies the type of object that is being lazily initialized.</typeparam>
+    public class Lazy<T>
+    {
+        private readonly object padlock = new object();
+        private readonly Func<T> createValue;
+        private bool isValueCreated;
+        private T value;
+
+        /// <summary>
+        /// Gets the lazily initialized value of the current Lazy{T} instance.
+        /// </summary>
+        public T Value
+        {
+            get
+            {
+                if (!isValueCreated)
+                {
+                    lock (padlock)
+                    {
+                        if (!isValueCreated)
+                        {
+                            value = createValue();
+                            isValueCreated = true;
+                        }
+                    }
+                }
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value that indicates whether a value has been created for this Lazy{T} instance.
+        /// </summary>
+        public bool IsValueCreated
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    return isValueCreated;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the Lazy{T} class.
+        /// </summary>
+        /// <param name="createValue">The delegate that produces the value when it is needed.</param>
+        public Lazy(Func<T> createValue)
+        {
+            if (createValue == null) throw new ArgumentNullException("createValue");
+
+            this.createValue = createValue;
+        }
+
+
+        /// <summary>
+        /// Creates and returns a string representation of the Lazy{T}.Value.
+        /// </summary>
+        /// <returns>The string representation of the Lazy{T}.Value property.</returns>
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
