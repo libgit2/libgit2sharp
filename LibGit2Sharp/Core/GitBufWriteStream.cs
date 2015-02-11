1adcff135099db01f794d50bfba8e7ae3ef27e1c
nulltoken <emeric.fermas@gmail.com>
2015-02-11 11:16:51 +0100

    Reduce the number of reallocations

--- LibGit2Sharp/Core/GitBufWriteStream.cs
+++ LibGit2Sharp/Core/GitBufWriteStream.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using LibGit2Sharp.Core.Handles;
 
 namespace LibGit2Sharp.Core
 {
@@ -10,6 +11,9 @@ namespace LibGit2Sharp.Core
         internal GitBufWriteStream(IntPtr gitBufPointer)
         {
             this.gitBufPointer = gitBufPointer;
+
+            //Preallocate the buffer
+            Proxy.git_buf_grow(gitBufPointer, 1024);
         }
 
         public override void Flush()
@@ -33,7 +37,29 @@ namespace LibGit2Sharp.Core
 
         public override void Write(byte[] buffer, int offset, int count)
         {
+            AutoGrowBuffer(count);
+
             Proxy.git_buf_put(gitBufPointer, buffer, offset, count);
+        }
+
+        private void AutoGrowBuffer(int count)
+        {
+            var gitBuf = gitBufPointer.MarshalAs<GitBuf>();
+
+            var asize = (uint)gitBuf.asize;
+            var size = (uint)gitBuf.size;
+
+            var isBufferLargeEnoughToHoldTheNewData = (asize - size) > count;
+            var filledBufferPercentage = (100.0 * size / asize);
+
+            if (isBufferLargeEnoughToHoldTheNewData && filledBufferPercentage < 90)
+            {
+                return;
+            }
+
+            var targetSize = (uint)(1.5 * (asize + count));
+
+            Proxy.git_buf_grow(gitBufPointer, targetSize);
         }
 
         public override bool CanRead
