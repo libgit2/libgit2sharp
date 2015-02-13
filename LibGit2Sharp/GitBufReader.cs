b3878f165e1c4327edafbc4436a98fa9cf73e90f
Amy Palamountain <amy@palamounta.in>
2015-02-13 11:57:34 -0800

    These types will be dead soon. But appveyor doesnt want them in this namespace.

--- LibGit2Sharp/GitBufReader.cs
+++ LibGit2Sharp/GitBufReader.cs
@@ -1,44 +1,11 @@
 using System;
 using System.Globalization;
 using System.IO;
-using System.Runtime.InteropServices;
+using LibGit2Sharp.Core;
 using LibGit2Sharp.Core.Handles;
 
-namespace LibGit2Sharp.Core
+namespace LibGit2Sharp
 {
-    /// <summary>
-    /// Writes data to a <see cref="GitBuf"/> pointer
-    /// </summary>
-    public class GitBufWriter 
-    {
-        private readonly IntPtr gitBufPointer;
-        private readonly GitBuf gitBuf;
-
-        internal GitBufWriter(IntPtr gitBufPointer)
-        {
-            this.gitBufPointer = gitBufPointer;
-            this.gitBuf = gitBufPointer.MarshalAs<GitBuf>();
-        }
-
-        /// <summary>
-        /// Write bytes to the <see cref="GitBuf"/> pointer
-        /// </summary>
-        /// <param name="bytes">The bytes to write</param>
-        public void Write(byte[] bytes)
-        {
-            IntPtr reverseBytesPointer = Marshal.AllocHGlobal(bytes.Length);
-            Marshal.Copy(bytes, 0, reverseBytesPointer, bytes.Length);
-
-            var size = (UIntPtr)bytes.LongLength;
-            var allocatedSize = (UIntPtr)bytes.LongLength + 1;
-            NativeMethods.git_buf_set(gitBuf, reverseBytesPointer, size);
-            gitBuf.size = size;
-            gitBuf.asize = allocatedSize;
-
-            Marshal.StructureToPtr(gitBuf, gitBufPointer, true);
-        }
-    }
-
     /// <summary>
     /// Reads data from a <see cref="GitBuf"/> pointer
     /// </summary>
