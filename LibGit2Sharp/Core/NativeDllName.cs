2322a82ebd1430faeb97ce65677846ad9cd00bb9
nulltoken <emeric.fermas@gmail.com>
2015-02-28 19:26:55 +0100

    fixup! update libgit2 to support streaming filters
    Update libgit2 to fb2f3a7
    
    https://github.com/libgit2/libgit2/compare/e0902fb...fb2f3a7

--- LibGit2Sharp/Core/NativeDllName.cs
+++ LibGit2Sharp/Core/NativeDllName.cs
@@ -1,7 +1,7 @@
 namespace LibGit2Sharp.Core
 {
-    internal static class NativeDllName
-    {
-        public const string Name = "git2-e0902fb";
-    }
+	internal static class NativeDllName
+	{
+		public const string Name = "git2-fb2f3a7";
+	}
 }
