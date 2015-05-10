32da01c5ff73d73f2865528521e39a8e740596d7
Brendan Forster <brendan@github.com>
2015-05-10 13:02:57 +0200

    Drop optional parameters in BlobExtensions.cs

--- LibGit2Sharp/BlobExtensions.cs
+++ LibGit2Sharp/BlobExtensions.cs
@@ -10,18 +10,41 @@ namespace LibGit2Sharp
     public static class BlobExtensions
     {
         /// <summary>
-        /// Gets the blob content decoded with the specified encoding,
-        /// or according to byte order marks, with UTF8 as fallback,
-        /// if <paramref name="encoding"/> is null.
+        /// Gets the blob content, decoded with UTF8 encoding if the encoding cannot be detected from the byte order mark
         /// </summary>
         /// <param name="blob">The blob for which the content will be returned.</param>
-        /// <param name="encoding">The encoding of the text. (default: detected or UTF8)</param>
         /// <returns>Blob content as text.</returns>
-        public static string GetContentText(this Blob blob, Encoding encoding = null)
+        public static string GetContentText(this Blob blob)
         {
             Ensure.ArgumentNotNull(blob, "blob");
 
+            return ReadToEnd(blob.GetContentStream(), null);
+        }
+
+        /// <summary>
+        /// Gets the blob content decoded with the specified encoding,
+        /// or according to byte order marks, or the specified encoding as a fallback
+        /// </summary>
+        /// <param name="blob">The blob for which the content will be returned.</param>
+        /// <param name="encoding">The encoding of the text to use, if it cannot be detected</param>
+        /// <returns>Blob content as text.</returns>
+        public static string GetContentText(this Blob blob, Encoding encoding)
+        {
+            Ensure.ArgumentNotNull(blob, "blob");
+            Ensure.ArgumentNotNull(encoding, "encoding");
+
             return ReadToEnd(blob.GetContentStream(), encoding);
+        }
+
+        /// <summary>
+        /// Gets the blob content, decoded with UTF8 encoding if the encoding cannot be detected
+        /// </summary>
+        /// <param name="blob">The blob for which the content will be returned.</param>
+        /// <param name="filteringOptions">Parameter controlling content filtering behavior</param>
+        /// <returns>Blob content as text.</returns>
+        public static string GetContentText(this Blob blob, FilteringOptions filteringOptions)
+        {
+            return blob.GetContentText(filteringOptions, null);
         }
 
         /// <summary>
@@ -34,7 +57,7 @@ namespace LibGit2Sharp
         /// <param name="filteringOptions">Parameter controlling content filtering behavior</param>
         /// <param name="encoding">The encoding of the text. (default: detected or UTF8)</param>
         /// <returns>Blob content as text.</returns>
-        public static string GetContentText(this Blob blob, FilteringOptions filteringOptions, Encoding encoding = null)
+        public static string GetContentText(this Blob blob, FilteringOptions filteringOptions, Encoding encoding)
         {
             Ensure.ArgumentNotNull(blob, "blob");
             Ensure.ArgumentNotNull(filteringOptions, "filteringOptions");
