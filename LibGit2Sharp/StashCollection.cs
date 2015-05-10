1f9d9ddb2b7b2591a9109eb42b9dca222333417d
Brendan Forster <brendan@github.com>
2015-05-10 12:59:07 +0200

    Drop optional parameters in StashCollection.cs

--- LibGit2Sharp/StashCollection.cs
+++ LibGit2Sharp/StashCollection.cs
@@ -80,10 +80,41 @@ namespace LibGit2Sharp
         /// Creates a stash with the specified message.
         /// </summary>
         /// <param name="stasher">The <see cref="Signature"/> of the user who stashes </param>
+        /// <returns>the newly created <see cref="Stash"/></returns>
+        public virtual Stash Add(Signature stasher)
+        {
+            return Add(stasher, null, StashModifiers.Default);
+        }
+        /// <summary>
+        /// Creates a stash with the specified message.
+        /// </summary>
+        /// <param name="stasher">The <see cref="Signature"/> of the user who stashes </param>
+        /// <param name="options">A combination of <see cref="StashModifiers"/> flags</param>
+        /// <returns>the newly created <see cref="Stash"/></returns>
+        public virtual Stash Add(Signature stasher, StashModifiers options)
+        {
+            return Add(stasher, null, options);
+        }
+
+        /// <summary>
+        /// Creates a stash with the specified message.
+        /// </summary>
+        /// <param name="stasher">The <see cref="Signature"/> of the user who stashes </param>
+        /// <param name="message">The message of the stash.</param>
+        /// <returns>the newly created <see cref="Stash"/></returns>
+        public virtual Stash Add(Signature stasher, string message)
+        {
+            return Add(stasher, message, StashModifiers.Default);
+        }
+
+        /// <summary>
+        /// Creates a stash with the specified message.
+        /// </summary>
+        /// <param name="stasher">The <see cref="Signature"/> of the user who stashes </param>
         /// <param name="message">The message of the stash.</param>
         /// <param name="options">A combination of <see cref="StashModifiers"/> flags</param>
         /// <returns>the newly created <see cref="Stash"/></returns>
-        public virtual Stash Add(Signature stasher, string message = null, StashModifiers options = StashModifiers.Default)
+        public virtual Stash Add(Signature stasher, string message, StashModifiers options)
         {
             Ensure.ArgumentNotNull(stasher, "stasher");
 
