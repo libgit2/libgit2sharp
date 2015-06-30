d608b25a7729f7f59d8458d6a97cc8e26325d8dd
SushiHangover <sushihangover@outlook.com>
2015-06-29 23:55:14 -0700

    Bump CI build requirements: * OS-X : 4.0.2 (latest public MDK package) * Linux: 4.0.1 (latest in debian main, non-pinned version)
    Added exe wrapper for CustomBuildTasks.dll to allow xplat project building with the following:
    * Xamarian Studio v5.9.x+ / MonoDevelop v5.9.x+
    * mdtool (matching version from above's IDE installation)
    * xbuild (from mono 4.0.2+ installation)

---
+++ Lib/CustomBuildTasks/CustomBuildTasksExe/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Utilities;
+using CustomBuildTasks;
+
+namespace CustomeBuildTasksExe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0] == "UniqueIdentifierTask")
+                {
+                    var guidTask = new GenerateUniqueIdentifierTask();
+                    guidTask.OutputFile = args.Length > 1 ? args[1] : Path.Combine(Environment.CurrentDirectory, "Core/UniqueIdentifier.cs");
+                    guidTask.Execute();
+                    Console.WriteLine("Generated: {0}", guidTask.OutputFile);
+                }
+                else if (args[0] == "NativeDllNameTask")
+                {
+                    string fileName;
+                    fileName = args.Length < 2 ? Directory.GetFiles("../packages", "libgit2_hash.txt", SearchOption.AllDirectories)[0] : args[1];
+                    var dllNameTask = new GenerateNativeDllNameTask();
+                    dllNameTask.InputHashFile = new TaskItem(fileName);
+                    dllNameTask.OutputFile = args.Length > 1 ? args[2] : Path.Combine(Environment.CurrentDirectory, "Core/NativeDllName.cs");
+                    dllNameTask.Execute();
+                    Console.WriteLine("Generated: {0}", dllNameTask.OutputFile);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("{0}: Unsupported Microsoft.Build.Utilities.Task Id supplied, no task executed.", args[0]);
+                }
+            }
+        }
+    }
+}
