9597543241a59e3761b3347f2d94dc43f1078291
Carlos Martín Nieto <cmn@dwim.me>
2015-08-21 12:42:23 +0200

    Create user/pass credentials from native

---
+++ LibGit2Sharp/Core/GitCredential.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibGit2Sharp.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal class GitCredential
+    {
+        public GitCredentialType credtype;
+        public IntPtr free;
+    }
+}
+
---
+++ LibGit2Sharp/Core/GitCredentialUserpass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LibGit2Sharp.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal class GitCredentialUserpass
+    {
+        public GitCredential parent;
+        public IntPtr username;
+        public IntPtr password;
+    }
+}
+
