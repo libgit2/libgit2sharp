2146e592ce5d44b6dee7239b327fd98e5317fa37
Edward Thomson <ethomson@github.com>
2016-06-27 12:11:23 -0400

    travis: use bundled nuget

--- build.libgit2sharp.sh
+++ build.libgit2sharp.sh
@@ -9,6 +9,7 @@ EXTRADEFINE="$1"
 # working directory in its library search path, so it works without this value.
 export LD_LIBRARY_PATH=.
 
+mono lib/nuget/nuget.exe restore
 xbuild CI/build.msbuild /target:Deploy /property:ExtraDefine="$EXTRADEFINE"
 
 exit $?
