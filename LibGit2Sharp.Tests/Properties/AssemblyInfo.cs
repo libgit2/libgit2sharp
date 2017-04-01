324f8dd55ce9fab0efa3b32960c155cc6f3deda2
Brandon Ording <bording@gmail.com>
2017-04-01 15:10:25 -0400

    Disable test parallelization to avoid test failures

--- LibGit2Sharp.Tests/Properties/AssemblyInfo.cs
+++ LibGit2Sharp.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -37,3 +38,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
