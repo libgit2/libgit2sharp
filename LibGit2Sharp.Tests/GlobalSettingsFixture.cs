7067eb5a21d32932fdfc421d580d9544ce12b5e8
Andrew Arnott <andrewarnott@gmail.com>
2017-04-24 23:10:35 -0700

    Update test comments and remove unnecessary check
    We have exactly two optional regex matching groups. Regex itself guarantees that the required groups were present, so the last half of this test that confirms that a hard-coded set of these groups were present is not testing libgit2sharp as much as it is testing regex itself, which is difficult to maintain and pointless, so I removed it rather than fixing it when I added the second optional group.

--- LibGit2Sharp.Tests/GlobalSettingsFixture.cs
+++ LibGit2Sharp.Tests/GlobalSettingsFixture.cs
@@ -19,41 +19,26 @@ namespace LibGit2Sharp.Tests
         public void CanRetrieveValidVersionString()
         {
             // Version string format is:
-            //      Major.Minor.Patch[-preDateTime]-LibGit2Sharp_abbrev_hash-libgit2_abbrev_hash (x86|x64 - features)
+            //      Major.Minor.Patch[-somePreleaseTag]-LibGit2Sharp_abbrev_hash-libgit2_abbrev_hash (x86|x64 - features)
             // Example output:
             //      "0.17.0[-pre20170914123547]-deadcafe-06d772d (x86 - Threads, Https)"
 
             string versionInfo = GlobalSettings.Version.ToString();
 
             // The GlobalSettings.Version returned string should contain :
-            //      version: '0.17.0[-pre20170914123547]' LibGit2Sharp version number.
+            //      version: '0.17.0[-somePreleaseTag][+gSomeGitCommit]' LibGit2Sharp version number.
             //      git2SharpHash:'unknown' ( when compiled from source ) else LibGit2Sharp library hash.
             //      git2hash: '06d772d' LibGit2 library hash.
             //      arch: 'x86' or 'x64' LibGit2 target.
             //      git2Features: 'Threads, Ssh' LibGit2 features compiled with.
-            string regex = @"^(?<version>\d{1,}\.\d{1,2}\.\d{1,3}(\+g[a-f0-9]{10})?)-(?<git2SharpHash>\w+)-(?<git2Hash>\w+) \((?<arch>\w+) - (?<git2Features>(?:\w*(?:, )*\w+)*)\)$";
+            string regex = @"^(?<version>\d+\.\d+\.\d+(-\w+)?(\+g[a-f0-9]{10})?)-(?<git2SharpHash>\w+)-(?<git2Hash>\w+) \((?<arch>\w+) - (?<git2Features>(?:\w*(?:, )*\w+)*)\)$";
 
             Assert.NotNull(versionInfo);
 
             Match regexResult = Regex.Match(versionInfo, regex);
 
             Assert.True(regexResult.Success, "The following version string format is enforced:" +
-                                             "Major.Minor.Patch[-preDateTime]-LibGit2Sharp_abbrev_hash-libgit2_abbrev_hash (x86|x64 - features)");
-
-            GroupCollection matchGroups = regexResult.Groups;
-
-            Assert.Equal(7, matchGroups.Count);
-
-            // Check that all groups are valid
-            for (int i = 0; i < matchGroups.Count; i++)
-            {
-                if (i == 1 || i == 2) // '-pre' segment is optional
-                {
-                    continue;
-                }
-
-                Assert.True(matchGroups[i].Success);
-            }
+                                             "Major.Minor.Patch[-somePreleaseTag][+gSomeGitCommit]-LibGit2Sharp_abbrev_hash-libgit2_abbrev_hash (x86|x64 - features)");
         }
 
         [Fact]
