0a3306562f3616f42e4d42b5472fa1de40eb5973
Brandon Ording <bording@gmail.com>
2017-08-02 00:19:09 -0400

    Cleanup

--- LibGit2Sharp/Properties/AssemblyInfo.cs
+++ LibGit2Sharp/Properties/AssemblyInfo.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyCopyright("Copyright © LibGit2Sharp contributors")]
 
 [assembly: CLSCompliant(true)]
 
