37cb13905f1667664bf6cd84b29fa4f72c286f6d
dazinator <darrell.tunnell@googlemail.com>
2017-10-25 01:09:52 +0100

    Fix for locating lib folder on netcore

--- LibGit2Sharp/GlobalSettings.cs
+++ LibGit2Sharp/GlobalSettings.cs
@@ -23,19 +23,30 @@ namespace LibGit2Sharp
         {
             if (Platform.OperatingSystem == OperatingSystemType.Windows)
             {
+               
+                string managedPath = string.Empty;
+
 #if DESKTOP
+                var assembly = Assembly.GetExecutingAssembly();
+                managedPath = assembly.CodeBase;
+                if (managedPath == null)
+                {
+                    managedPath = assembly.Location;
+                }
+#else
+                var assembly = typeof(GlobalSettings).GetTypeInfo().Assembly;
+                managedPath = (string)assembly.GetType().GetProperty("Location").GetValue(assembly);              
+#endif
+
+
                 /* Assembly.CodeBase is not actually a correctly formatted
                  * URI.  It's merely prefixed with `file:///` and has its
                  * backslashes flipped.  This is superior to EscapedCodeBase,
                  * which does not correctly escape things, and ambiguates a
                  * space (%20) with a literal `%20` in the path.  Sigh.
                  */
-                var managedPath = Assembly.GetExecutingAssembly().CodeBase;
-                if (managedPath == null)
-                {
-                    managedPath = Assembly.GetExecutingAssembly().Location;
-                }
-                else if (managedPath.StartsWith("file:///"))
+
+                if (managedPath.StartsWith("file:///"))
                 {
                     managedPath = managedPath.Substring(8).Replace('/', '\\');
                 }
@@ -45,10 +56,6 @@ namespace LibGit2Sharp
                 }
 
                 managedPath = Path.GetDirectoryName(managedPath);
-#else
-                string managedPath = AppContext.BaseDirectory;
-#endif
-
                 nativeLibraryPath = Path.Combine(managedPath, "lib", "win32");
             }
 
