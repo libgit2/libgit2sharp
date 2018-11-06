222d4c432303063aee5f7b0fbb603db102a848f7
Mike Minns <mminns@itofinity.co.uk>
2018-11-06 20:31:07 +0000

    Issue-1471 Added code comments to WorktreeLock

--- LibGit2Sharp/WorktreeLock.cs
+++ LibGit2Sharp/WorktreeLock.cs
@@ -6,20 +6,38 @@ using System.Text;
 
 namespace LibGit2Sharp
 {
+    /// <summary>
+    ///     Represents the lock state of a Worktree
+    /// </summary>
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class WorktreeLock
     {
+        /// <summary>
+        ///     Creates a new instance of <see cref="WorktreeLock"/> with default, unlocked, state
+        /// </summary>
         public WorktreeLock() : this(false, null)
         {
 
         }
 
+        /// <summary>
+        ///     Creates a new instance of <see cref="WorktreeLock"/>
+        /// </summary>
+        /// <param name="isLocked">the locked state</param>
+        /// <param name="reason">the reason given for the lock</param>
         public WorktreeLock(bool isLocked, string reason)
         {
             IsLocked = isLocked;
             Reason = reason;
         }
+        /// <summary>
+        ///     Gets a flag indicating if the worktree is locked
+        /// </summary>
         public virtual bool IsLocked { get; }
+
+        /// <summary>
+        ///     Gets the reason, if set, for the lock
+        /// </summary>
         public virtual string Reason { get; }
 
         private string DebuggerDisplay
