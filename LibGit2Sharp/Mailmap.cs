11c8e0edc1e8be980c82ef7c78d0e5fc126c82bc
Viktor Semenov <viktor@boostlingo.com>
2019-08-13 17:45:49 +0900

    Add mailmap managed class

---
+++ LibGit2Sharp/Mailmap.cs
@@ -0,0 +1,82 @@
+using System;
+using LibGit2Sharp.Core;
+using LibGit2Sharp.Core.Handles;
+
+namespace LibGit2Sharp
+{
+    /// <summary>
+    /// Representation of .mailmap file state.
+    /// </summary>
+    public class Mailmap : IDisposable
+    {
+        private readonly Repository repo;
+        private readonly MailmapHandle mailmapHandle;
+
+        internal Mailmap(Repository repo)
+        {
+            this.repo = repo;
+
+            mailmapHandle = Proxy.git_mailmap_from_repository(repo.Handle);
+        }
+
+        /// <summary>
+        /// Resolve a name and email to the corresponding real email.
+        /// </summary>
+        /// <param name="name">the name to look up</param>
+        /// <param name="email">the email to look up</param>
+        /// <returns>the real email</returns>
+        public string ResolveRealEmail(string name, string email)
+        {
+            Proxy.git_mailmap_resolve(out var realName, out var realEmail, mailmapHandle, name, email);
+
+            return realEmail;
+        }
+
+        /// <summary>
+        /// Resolve a name and email to the corresponding real name.
+        /// </summary>
+        /// <param name="name">the name to look up</param>
+        /// <param name="email">the email to look up</param>
+        /// <returns>the real name</returns>
+        public string ResolveRealName(string name, string email)
+        {
+            Proxy.git_mailmap_resolve(out var realName, out var realEmail, mailmapHandle, name, email);
+
+            return realName;
+        }
+
+        /// <summary>
+        /// Resolve a signature to use real names and emails with a mailmap.
+        /// </summary>
+        /// <param name="signature">signature to resolve</param>
+        /// <returns>new signature</returns>
+        public unsafe Signature ResolveSignature(Signature signature)
+        {
+            using (var signatureHandle = Proxy.git_mailmap_resolve_signature(mailmapHandle, signature.BuildHandle()))
+            {
+                return new Signature(signatureHandle);
+            }
+        }
+
+        #region IDisposable Members
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            mailmapHandle.SafeDispose();
+        }
+
+        #endregion
+    }
+}
