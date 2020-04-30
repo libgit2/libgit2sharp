3c777dc40211f6208edb3d2f3d06ea47ccd455c7
Stijn Rutten <Stijn.Rutten@infosupport.com>
2020-04-30 14:03:16 +0200

    Set deleted and added lines in patch printcallback

--- LibGit2Sharp/Patch.cs
+++ LibGit2Sharp/Patch.cs
@@ -68,6 +68,11 @@ namespace LibGit2Sharp
             PatchEntryChanges currentChange = this[filePath];
             string prefix = string.Empty;
 
+            string decodedContent = LaxUtf8Marshaler.FromNative(line.content, (int)line.contentLen);
+
+            currentChange.AddedLines = new List<Line>();
+            currentChange.DeletedLines = new List<Line>();
+
             switch (line.lineOrigin)
             {
                 case GitDiffLineOrigin.GIT_DIFF_LINE_CONTEXT:
@@ -77,12 +82,14 @@ namespace LibGit2Sharp
                 case GitDiffLineOrigin.GIT_DIFF_LINE_ADDITION:
                     linesAdded++;
                     currentChange.LinesAdded++;
+                    currentChange.AddedLines.Add(new Line(line.NewLineNo, decodedContent));
                     prefix = "+";
                     break;
 
                 case GitDiffLineOrigin.GIT_DIFF_LINE_DELETION:
                     linesDeleted++;
                     currentChange.LinesDeleted++;
+                    currentChange.DeletedLines.Add(new Line(line.OldLineNo, decodedContent));
                     prefix = "-";
                     break;
             }
