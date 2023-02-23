70d62d5adf6ac951d756d115c6b34b719947a83a
Edward Thomson <ethomson@edwardthomson.com>
2023-02-23 11:33:36 -0800

    LibGit2Sharp: update to libgit2 v1.6.2

--- LibGit2Sharp/Core/GitPushOptions.cs
+++ LibGit2Sharp/Core/GitPushOptions.cs
@@ -9,6 +9,7 @@ namespace LibGit2Sharp.Core
         public int PackbuilderDegreeOfParallelism;
         public GitRemoteCallbacks RemoteCallbacks;
         public GitProxyOptions ProxyOptions;
+        public RemoteRedirectMode FollowRedirects = RemoteRedirectMode.Auto;
         public GitStrArrayManaged CustomHeaders;
     }
 }
---
+++ LibGit2Sharp/RemoteRedirectMode.cs
@@ -0,0 +1,28 @@
+﻿namespace LibGit2Sharp
+{
+    /// <summary>
+    /// Remote redirection settings; wehther redirects to another
+    /// host are permitted. By default, git will follow a redirect
+    /// on the initial request (`/info/refs`) but not subsequent
+    /// requests.
+    /// </summary>
+    public enum RemoteRedirectMode
+    {
+        /// <summary>
+        /// Do not follow any off-site redirects at any stage of
+        /// the fetch or push.
+        /// </summary>
+        None = 0, // GIT_REMOTE_REDIRECT_NONE
+
+        /// <summary>
+        /// Allow off-site redirects only upon the initial
+        /// request. This is the default.
+        /// </summary>
+        Auto,     // GIT_REMOTE_REDIRECT_INITIAL
+
+        /// <summary>
+        /// Allow redirects at any stage in the fetch or push.
+        /// </summary>
+        All       // GIT_REMOTE_REDIRECT_ALL
+    }
+}
