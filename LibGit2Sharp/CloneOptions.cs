db161ee8baad1a8969c1e6a150ab5618e84521c6
Brandon Ording <bording@gmail.com>
2023-11-25 19:16:49 -0500

    No reason to derive from FetchOptionsBase and have FetchOptions property

--- LibGit2Sharp/CloneOptions.cs
+++ LibGit2Sharp/CloneOptions.cs
@@ -6,7 +6,7 @@ namespace LibGit2Sharp
     /// <summary>
     /// Options to define clone behaviour
     /// </summary>
-    public sealed class CloneOptions : FetchOptionsBase, IConvertableToGitCheckoutOpts
+    public sealed class CloneOptions : IConvertableToGitCheckoutOpts
     {
         /// <summary>
         /// Creates default <see cref="CloneOptions"/> for a non-bare clone
