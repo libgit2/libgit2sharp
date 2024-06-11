bd0c16e5069bbecdbbbd7f172dd034f454c6a6c4
Andrei Balint <andrei.balint@uipath.com>
2024-06-11 14:46:19 +0300

    select schannel ssl backend if configured in .gitconfig globally
    part of STUD-7_0_3_0_1

---
+++ LibGit2Sharp/Core/ConfigurationFileReader.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+using System.Runtime.InteropServices;
+using System.Text;
+
+internal sealed class ConfigurationFileReader
+{
+    private readonly string _path;
+
+    [DllImport("kernel32", CharSet = CharSet.Unicode)]
+    private static extern int GetPrivateProfileString(string section, string key, string defaultValue, StringBuilder result, int size, string filePath);
+
+    public ConfigurationFileReader(string path)
+    {
+        _path = path;
+    }
+
+    public string Read(string section, string key)
+    {
+        var result = new StringBuilder(255);
+        GetPrivateProfileString(section, key, "", result, 255, _path);
+        return result.ToString();
+    }
+}
